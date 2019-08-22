630fb5f22e70bd6f85eacbc131998e24ff07e476
unknown <calvin@logique.co.id>
2019-08-22 09:21:06 +0700

    finished about us page

---
+++ about.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <link rel="stylesheet" href="css/slick.css" />
+    <link rel="stylesheet" href="css/slick-theme.css" />
+    <link rel="stylesheet" href="css/header-footer.css" />
+    <link rel="stylesheet" href="css/style-about.css" />
+    <title>Features - Dokodemo Kerja</title>
+</head>
+
+<body>
+    <?php include "header.php" ?>
+    <section class="top-content">
+        <img src="img/about/bg.jpg" class="img-bg" />
+        <div class="container">
+            <div class="row align-items-end">
+                <div class="col-lg-6 about-us-left">
+                    <h1>ABOUT US</h1>
+                    <p class="about-us-desc">Dokodemo-Kerja is a Human Resource tool developed by PT Logique Digital Indonesia, a digital solution company based in Jakarta, Indonesia. This program was made to help employees achieve their best potential.
+                        <br /><br />
+                        The name Dokodemo-Kerja comes from 2 languages: "Dokodemo": anywhere (Japanese) and "Kerja": to work (Indonesian), which translates into "Work Anywhere". It means that with this program, you or your employee would not be restrained to a cubicle or an office at all times.</p>
+                    <p class="about-us-contact">We use the program in our company, and if you wonder how it works in real life, do <a href="contact-us.php">CONTACT US</a> and we will explain how we utilize this tool.</p>
+                    <img src="img/about/dokodemo-about.png" class="img-dokodemo-about" />
+                </div>
+                <div class="col-lg-6 about-us-right">
+                    <h1>WHY DOKODEMO-KERJA?</h1>
+                    <p>The purpose of this program is to promote healthy and productive work culture aimed at highly populated city. In Jakarta, Traffic Jam has become a huge problem, and arriving late at the office has became a "millenial culture". We are aiming to change that.
+                        <br><br>
+                        Additionally, if a big office space seems too take up too much of your profit away, it might be time to consider an alternative. Employees who live more than 10km away from the office could really use a break from time to time, without affecting their job performance.
+                        <br><br>
+                        With a proper work rotation, Dokodemo-Kerja could be a decent solution to retain current workers, and promote a healthy work environment.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="to-whom">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 offset-md-3">
+                    <div class="hr-managers">
+                        <h1>To Whom?</h1>
+                        <h2>HR AND MANAGERS</h2>
+                        <p class="fw-semibold">Dokodemo-Kerja will give you total control of your employee's activity.
+                            You don't need to know where they are, but you know what they are doing.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="hr-manager-benefits">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 benefits-item">
+                    <h3>1. Save up on the cost of huge office spaces</h3>
+                    <p>You can rotate your employee's working days in and out of the office if an expansion is not possible. Some employees might not feel too comfortable working in a cubicle, and works more productively outside. Working remotely could also be implemented as a reward system, in addition to the existing benefits.</p>
+                </div>
+                <div class="col-md-6 benefits-item">
+                    <h3>2. Determine who your best employee is more objectivelys</h3>
+                    <p>
+                        Your best employee would want to be recognised for his/her work. Those who puts in more time, completes more tasks, and doesn't slack off as much would be rewarded more than those who visibly neglects their job.
+                    </p>
+                </div>
+                <div class="col-md-6 benefits-item">
+                    <h3>3. Save up millions on lost time</h3>
+                    <p>
+                        Are your employees really working according to the minimum time requirement? Are you really getting what you paid for? By using Dokodemo-Kerja, you can see how long employees take to complete a task, and help you make a better project plan.
+                    </p>
+                </div>
+                <div class="col-md-6 benefits-item">
+                    <h3>4. Increase employee retention</h3>
+                    <p>
+                        Employee retention is an important part of an organization&apos;s growth. By rewarding all the right employees, it would reduce the resignation rate. Dokodemo-Kerja can help you determine the employees worth keeping.
+                    </p>
+                </div>
+                <div class="col-md-10 offset-md-1">
+                    <p class="text-center fw-semibold pt-4">The most common reasons for employees to quit are Long commutes, Lack of work/life balance, and Poor salary/benefits. By letting them work remotely on their terms, they will see that there is an actual benefit working for you.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="employee-benefits">
+        <div class="container">
+            <h2 class="text-center fs-30">EMPLOYEES</h2>
+            <div class="row">
+                <div class="col-md-10 offset-md-1">
+                    <p class="fw-semibold text-center">In times of rapid growth, keeping a work-life balance has became a challenge. Here are the common types of problems that might hinder an employee's working-hour, and how Dokodemo-Kerja solves some of those problems.</p>
+                </div>
+                <div class="col-md-6 employee-benefits-item">
+                    <img src="img/about/chair.png" />
+                    <div class="employee-benefits-desc pl-4">
+                        <h3>1.Parental Leave</h3>
+                        <p>
+
+                            One of the most common concern for employers and employees alike is maternity leave. Employees will be gone for at least 6 weeks or more, and even then, would they still have their job? Using Dokodemo-Kerja, the employee can rest assured, knowing that they can still keep their job, and work from the comfort of their own home.</p>
+                    </div>
+                </div>
+                <div class="col-md-6 employee-benefits-item">
+                    <img src="img/about/first_aid_kit.png" />
+                    <div class="employee-benefits-desc pl-4">
+                        <h3>2.Medical Leave</h3>
+                        <p>
+
+                            Some employees might feel pressured to go to the office despite their medical illness in fear of losing their job. There have been many cases where people are unwilling to take &quot;sick leave&quot; and go to the office with high fever or a cold, disregard their, and their co-worker&apos;s health. Some people even fall into medical debts for losing their job due to a temporary disability.
+                            <br><strong>By using this program, they can work from home, and in a hospital as they or their family members get treated</strong></p>
+                    </div>
+                </div>
+                <div class="col-md-6 employee-benefits-item">
+                    <img src="img/about/checklist.png" />
+                    <div class="employee-benefits-desc pl-4">
+                        <h3>3. Taking care of Important Documents
+                        </h3>
+                        <p>
+                            Everyone has a special circumstance as to why they can't go to the office. Maybe they need to take care of their child's school administration, register their driver's licence, open up a bank account, or maybe to extend their passport's validity. A lot of these activity should be done in the weekdays. They could be using that time to work remotely instead of spending it sitting in a waiting room for hours.</p>
+                    </div>
+                </div>
+                <div class="col-md-6 employee-benefits-item">
+                    <img src="img/about/coffee.png" />
+                    <div class="employee-benefits-desc pl-4">
+                        <h3>4.Break Time</h3>
+                        <p>
+                            It is perhaps a bit unfair to treat every employee equally if half of them spends most of their work time smoking, watching entertainment videos, taking hours of lunch breaks, or pray longer than necessary. With Dokodemo-Kerja, you can accurately track their breaks and working hours, as they can pause the time tracker when they are not really working.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="read-testimony">
+        <h3>Wonder what other user are saying?</h3>
+        <a href="/" class="btn-read-testimony">
+            Read user testimony
+        </a>
+    </section>
+    <?php include "footer.php" ?>
+</body>
