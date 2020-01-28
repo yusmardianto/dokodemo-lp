388b116cbad85b3a446db926f4665670bcbd30ee
Faiq Naufal <nauval@logique.co.id>
2020-01-28 12:24:34 +0700

    updated policy and terms page content

--- terms.php
+++ terms.php
@@ -132,100 +132,154 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     </section>
     <!-- end of req online meeting popup -->
     <section class="hr-manager-benefits">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12 benefits-item privacy-title">
-                    <h2>Introduction</h2>
-                    <p>These Website Standard Terms and Conditions written on this webpage shall manage your use of our (PT Logique Digital Indonesia) website, Dokodemo-Kerja accessible at dokodemo-kerja.com.</p>
-                    <p>These Terms will be applied fully and affect your use of this Website. By using this Website, you agreed to accept all terms and conditions written in here. You must not use this Website if you disagree with any of these Website Standard Terms and Conditions. </p>
-                </div>
-                <div class="col-md-12 benefits-item privacy-title">
-                    <h2>Intellectual Property Rights</h2>
-                    <p>
-                      Other than the content you own, under these Terms, PT Logique Digital Indonesia (the company what develops Dokodemo-Kerja) and/or its licensors own all the intellectual property rights and materials contained in this Website.
-                    </p>
-                    <p>You are granted a limited license only for purposes of viewing the material contained on this Website.</p>
-                </div>
-                <div class="col-md-12 benefits-item privacy-title">
-                    <h2>Personal Information You Provide to Us</h2>
-                    <p>
-                      We collect Personal Information that you submit to us voluntarily. The Personal Information we collect includes, but is not limited to, the following: name, mailing address, web address, telephone number, company name, e-mail address, and information contained in any e-mail or other communication you send to us or information you give to us in any other way.
-                    </p>
-                </div>
-                <div class="col-md-12 benefits-item privacy-title">
-                    <h2>Restrictions</h2>
-                    <p>
-                      You are specifically restricted from all of the following:
-                    </p>
-                    <ul>
-                        <li>publishing any Website material in any other media without permission (from PT Logique Digital Indonesia);</li>
-                        <li>selling, sublicensing and/or otherwise commercializing any Website material;</li>
-                        <li>using this Website in any way that is or may be damaging to this Website;</li>
-                        <li>using this Website in any way that impacts user access to this Website;</li>
-                        <li>using this Website contrary to applicable laws and regulations, or in any way may cause harm to the Website, or to any person or business entity;</li>
-                        <li>engaging in any data mining, data harvesting, data extracting or any other similar activity in relation to this Website;</li>
-                        <li>using this Website to engage in any advertising or marketing.</li>
-                    </ul>
-                    <p>Certain areas of this Website are restricted from being access by you and PT Logique Digital Indonesia may further restrict access by you to any areas of this Website, at any time, in absolute discretion. Any user ID and password you may have for this Website are confidential and you must maintain confidentiality as well.</p>
-                </div>
-                <div class="col-md-12 benefits-item privacy-title">
-                  <h2>Your Content</h2>
-                  <p>
-                    In these Website Standard Terms and Conditions, "Your Content" shall mean any audio, video text, images or other material you choose to display on this Website. By displaying Your Content, you grant PT Logique Digital Indonesia a non-exclusive, worldwide irrevocable, sub-licensable license to use, reproduce, adapt, publish, translate and distribute it in any and all media.
-                  </p>
-                  <p>Your Content must be your own and must not be invading any third-party’s rights. PT Logique Digital Indonesia reserves the right to remove any of Your Content from this Website at any time without notice.</p>
-                </div>
-                <div class="col-md-12 benefits-item privacy-title">
-                  <h2>No Warranties</h2>
-                  <p>
-                    This Website is provided "as is," with all faults, and PT Logique Digital Indonesia express no representations or warranties, of any kind related to this Website or the materials contained on this Website. Also, nothing contained on this Website shall be interpreted as advising you.
-                  </p>
-                </div>
-                <div class="col-md-12 benefits-item privacy-title">
-                  <h2>Limitation of liability</h2>
-                  <p>
-                    In no event shall PT Logique Digital Indonesia, nor any of its officers, directors and employees, shall be held liable for anything arising out of or in any way connected with your use of this Website whether such liability is under contract.  PT Logique Digital Indonesia, including its officers, directors and employees shall not be held liable for any indirect, consequential or special liability arising out of or in any way related to your use of this Website.
-                  </p>
-                </div>
-                <div class="col-md-12 benefits-item privacy-title">
-                  <h2>Indemnification</h2>
-                  <p>
-                    You hereby indemnify to the fullest extent PT Logique Digital Indonesia from and against any and/or all liabilities, costs, demands, causes of action, damages and expenses arising in any way related to your breach of any of the provisions of these Terms.
-                  </p>
-                </div>
-                <div class="col-md-12 benefits-item privacy-title">
-                  <h2>Severability</h2>
-                  <p>
-                    If any provision of these Terms is found to be invalid under any applicable law, such provisions shall be deleted without affecting the remaining provisions herein.
-                  </p>
-                </div>
-                <div class="col-md-12 benefits-item privacy-title">
-                  <h2>Variation of Terms</h2>
-                  <p>
-                    PT Logique Digital Indonesia is permitted to revise these Terms at any time as it sees fit, and by using this Website you are expected to review these Terms on a regular basis.
-                  </p>
-                </div>
-                <div class="col-md-12 benefits-item privacy-title">
-                  <h2>Assignment</h2>
-                  <p>
-                    The PT Logique Digital Indonesia is allowed to assign, transfer, and subcontract its rights and/or obligations under these Terms without any notification. However, you are not allowed to assign, transfer, or subcontract any of your rights and/or obligations under these Terms.
-                  </p>
-                </div>
-                <div class="col-md-12 benefits-item privacy-title">
-                  <h2>Entire Agreement</h2>
-                  <p>
-                    These Terms constitute the entire agreement between PT Logique Digital Indonesia and you in relation to your use of this Website, subscription of our service, and supersede all prior agreements and understandings.
-                  </p>
-                  <p>To add to the terms stated in the "Subscribing Procedure" page, PT Logique Digital Indonesia may add or subtract any of the content without prior warning. New changes will only be applicable to the subscribers after a new Purchase Order is issued.</p>
-                </div>
-                <div class="col-md-12 benefits-item privacy-title">
-                  <h2>Governing Law & Jurisdiction</h2>
-                  <p>
-                    These Terms will be governed by and interpreted in accordance with the laws of the Republic of Indonesia, and you submit to the non-exclusive jurisdiction of the country and federal courts in Indonesia for the resolution of any disputes.
-                  </p>
-                </div>
-            </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 benefits-item privacy-title">
+            <h2>Introduction</h2>
+            <p class="text-justify">
+              The Standard Terms and Conditions outlined on this webpage should inform your use of “Dokodemo Kerja”, the
+              details of which are accessible at <a href="https://dokodemo-kerja.com"
+                target="_blank">https://dokodemo-kerja.com</a>. Further, the Service “Dokodemo Kerja”
+              (provided by PT Logique Digital Indonesia) detailed within this webpage will hereinafter collectively be
+              referred to as “The Services.”
+            </p>
+            <p class="text-justify">
+              These Terms will be fully applied in regards to your use of The Services. By using The Services and its
+              features, you have agreed to accept all the Terms and Conditions outlined here. You must not use The
+              Services if you disagree with any of the stated Standard Terms and Conditions. These Terms and Conditions
+              have been generated with the help of <a href="https://www.termsandcondiitionssample.com" target="_blank"
+                rel="noreferrer">Terms And
+                Conditions Template</a>
+            </p>
+          </div>
+          <div class="col-md-12 benefits-item privacy-title">
+            <h2>Intellectual Property Rights</h2>
+            <p class="text-justify">
+              Under these Terms, PT Logique Digital Indonesia (the Service Provider) and its licensors own all the
+              intellectual property rights and materials contained within The Services.
+            </p>
+            <p class="text-justify">
+              You are granted a limited license based on the stipulations outlined in Dokodemo-Kerja’s Terms of Use, which
+              can be found within The Service’s application form.
+            </p>
+          </div>
+          <div class="col-md-12 benefits-item privacy-title">
+            <h2>Restrictions</h2>
+            <p class="subscribe-header-content text-justify mb-0">
+              You are specifically restricted from engaging in the following actions:
+            </p>
+            <ul>
+              <li>
+                Unless otherwise consented by PT Logique Digital Indonesia, the Customer may not allow individual User
+                Account Information to be shared by others.
+              </li>
+              <li>
+                Publishing any of The Service’s materials through any other media without permission from PT Logique
+                Digital Indonesia;
+              </li>
+              <li>
+                Selling, sublicensing and/or otherwise commercializing any of The Services’ materials;
+              </li>
+              <li>
+                Using The Services in any way that is or may be damaging to PT Logique Digital Indonesia;
+              </li>
+              <li>
+                Using The Services in any way that may impact user accessibility regarding The Services;
+              </li>
+              <li>
+                Using The Services in any way that might defy applicable laws and regulations, or in any way that might
+                cause harm to the Service Provider (PT Logique Digital Indonesia), or to any person or business entity;
+              </li>
+              <li>
+                Engage in data mining, data harvesting, data extraction or any other similar activity through the use of
+                The Services;
+              </li>
+              <li>
+                Using The Services to engage in any form of advertising or marketing
+              </li>
+            </ul>
+            <p>
+              Certain features of The Services are restricted from being accessed by you, and PT Logique Digital Indonesia
+              may further restrict your access to any of its features at any time, at its own discretion. Any user
+              IDs/Passwords you may have for The Services provided are confidential and you must thereby maintain the
+              confidentiality of this information.
+            </p>
+          </div>
+          <div class="col-md-12 benefits-item privacy-title">
+            <h2>Your Content</h2>
+            <p class="text-justify">
+              In this Standard Terms and Conditions, "Your Content" refers to Customer Data received through the
+              application (The Services). By using The Services which have been provided by PT Logique Indonesia, you
+              grant PT Logique Digital Indonesia a non-exclusive, universally irrevocable, sub-licensable license to use,
+              reproduce, adapt, publish, translate and distribute it in any and all media.
+            </p>
+          </div>
+          <div class="col-md-12 benefits-item privacy-title">
+            <h2>No warranties</h2>
+            <p class="text-justify">
+              The Services are provided "as is," with all faults, and PT Logique Digital Indonesia express no representations or warranties of any kind related to The Services or the materials contained within it.
+            </p>
+          </div>
+          <div class="col-md-12 benefits-item privacy-title">
+            <h2>Limitation of liability</h2>
+            <p class="text-justify">
+              In no event shall PT Logique Digital Indonesia, nor any of its officers, directors and employees, be held
+              liable for anything arising out of or in any way connected with your use of The Services. PT Logique Digital
+              Indonesia, including its officers, directors and employees shall not be held liable for any indirect,
+              consequential or special liability arising out of or in any way related to your use of The Services.
+            </p>
+          </div>
+          <div class="col-md-12 benefits-item privacy-title">
+            <h2>Indemnification</h2>
+            <p class="text-justify">
+              You hereby indemnify to the fullest extent PT Logique Digital Indonesia from and against any and/or all
+              liabilities, costs, demands, causes of action, damages and expenses arising in any way related to your
+              breach of any of the provisions of these Terms.
+            </p>
+          </div>
+          <div class="col-md-12 benefits-item privacy-title">
+            <h2>Severability</h2>
+            <p class="text-justify">
+              If any provision found within the Terms outlined here has been found to be invalid under any applicable law,
+              such provisions shall be deleted without affecting the remaining provisions herein.
+            </p>
+          </div>
+          <div class="col-md-12 benefits-item privacy-title">
+            <h2>Variation of Terms</h2>
+            <p class="text-justify">
+              PT Logique Digital Indonesia is permitted to revise these Terms at any time as it sees fit, and by using The
+              Services, you are expected to review these Terms on a regular basis.
+            </p>
+          </div>
+          <div class="col-md-12 benefits-item privacy-title">
+            <h2>Assignment</h2>
+            <p class="text-justify">
+              PT Logique Digital Indonesia is allowed to assign, transfer, and subcontract its rights and/or obligations
+              under these Terms without any notification. However, you are not allowed to assign, transfer, or subcontract
+              any of your rights and/or obligations under these Terms.
+            </p>
+          </div>
+          <div class="col-md-12 benefits-item privacy-title">
+            <h2>Entire Agreement</h2>
+            <p class="text-justify">
+              These Terms constitute the entire agreement between PT Logique Digital Indonesia and you in relation to your
+              use of The Services, subscription of our service, and supersede all prior agreements and understandings.
+            </p>
+            <p class="text-justify">
+              PT Logique Digital Indonesia may add or subtract any of the content without prior warning. New changes will
+              only be applicable to subscribers after a new Purchase Order is issued.
+            </p>
+          </div>
+          <div class="col-md-12 benefits-item privacy-title">
+            <h2>Governing Law & Jurisdiction</h2>
+            <p class="text-justify">
+              These Terms will be governed by and interpreted in accordance with the laws of the Republic of Indonesia,
+              and you hereby submit to the non-exclusive jurisdiction of the country and the federal courts of Indonesia
+              for the resolution of any disputes.
+            </p>
+          </div>
         </div>
+      </div>
     </section>
     <!-- <section class="read-testimony">
         <h3>Wonder what other user are saying?</h3>
