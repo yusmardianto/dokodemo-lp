9f413f00991992c9ddd7d53a681a1cb4aa802f28
Bagas Ranggen <bagas@logique.co.id>
2020-05-12 13:11:01 +0700

    fix typo

--- trial.php
+++ trial.php
@@ -84,7 +84,7 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 		<img src="/img/home/arrow-down.svg" class="pt-4 pb-3" width="20" alt=""/>
 		<a href="https://demo.dokodemo-kerja.com" class="btn btn-trial-contact bold uppercase" target="_blank" rel="noopener noreferrer" id="btn-demo">Admin Demo</a>
 		<a href="https://dokodemo-kerja.com/pdf/manual-guide-ENG.pdf" target="_blank" rel="noopener noreferrer">
-			<p class="trial-header-content">For staff demo, please follow the instruction bellow.</p>
+			<p class="trial-header-content">For staff demo, please follow the instruction below.</p>
 		</a>
 	</section>
 
