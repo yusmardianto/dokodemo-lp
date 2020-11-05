690d6c87a6dd6d19569bf3568d1b75a0aecc8ce8
Bagas Ranggen <bagas@logique.co.id>
2020-11-05 17:36:26 +0700

    basic layout for help page

---
+++ help.php
@@ -0,0 +1,194 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<meta name="description" itemprop="description" content="LOGIQUE Digital Indonesia provides digital solutions for human resources through remote working software, Dokodemo-Kerja, designed for reducing costs. ">
+    <meta name="keywords" content="LOGIQUE Digital Indonesia, Dokodemo-Kerja, digital solutions, human resources, remote working software, remote working, remote work, reducing costs, reduce costs, Nicholas Bloom, Working from home">
+    <meta name="author" content="PT. LOGIQUE Digital Indonesia">
+	<meta name="google-site-verification" content="Xc9SFJQZdKYLsrWvrxEjLLTA-nyrnNPQTXQoMxs4gKI" />
+    <meta property="og:title" content="Help Dokodemo-Kerja | Remote Work Tracking Tool for HR">
+    <meta property="og:type" content="website">
+    <meta property="og:site_name" content="Dokodemo-Kerja">
+    <meta property="og:url" content="https://dokodemo-kerja.com">
+    <meta property="og:image" content="https://dokodemo-kerja.com/img/og-img/about.jpg">
+    <meta property="og:description" content="Dokodemo-Kerja is a Human Resource tool developed by PT. LOGIQUE Digital Indonesia, a company providing Digital Solutions, based in Jakarta, Indonesia. Dokodemo-Kerja was designed to promote healthier and more productive work cultures in a way that benefits both the Company and Employees alike.">
+    <link rel="stylesheet" href="/css/bootstrap.min.css" />
+    <link href="/fonts/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="/css/slick.css" />
+    <link rel="stylesheet" href="/css/slick-theme.css" />
+    <link rel="stylesheet" href="/css/header-footer.css" />
+    <link rel="stylesheet" href="/css/style-help.css" />
+	<link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+    <title>About Dokodemo-Kerja | Cloud System for HR</title>
+    <script src="https://www.googleoptimize.com/optimize.js?id=OPT-P232K37"></script>
+	<!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-WT4MCDL');</script>
+	<!-- Facebook Pixel Code -->
+	<script>
+	  !function(f,b,e,v,n,t,s)
+	  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+	  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+	  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+	  n.queue=[];t=b.createElement(e);t.async=!0;
+	  t.src=v;s=b.getElementsByTagName(e)[0];
+	  s.parentNode.insertBefore(t,s)}(window, document,'script',
+	  'https://connect.facebook.net/en_US/fbevents.js');
+	  fbq('init', '713777762291663');
+	  fbq('track', 'PageView');
+	</script>
+	<noscript><img height="1" width="1" style="display:none"
+	  src="https://www.facebook.com/tr?id=713777762291663&ev=PageView&noscript=1"
+	/></noscript>
+	<!-- End Facebook Pixel Code -->
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-146404615-1"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+
+	  gtag('config', 'UA-146404615-1');
+	</script>
+</head>
+<?php $page = 'help'; ?>
+<body>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WT4MCDL"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+    <?php include "header.php" ?>
+
+    <div class="my-5 help">
+    
+        <section class="help__explain">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="explain__topic">
+                            <ul class="d-flex flex-column nav" role="tablist" id="accordion">
+                                <li class="nav-item" data-toggle="collapse" href="#">
+                                    <a class="nav-link active" id="" data-toggle="tab" href="#help_intro" role="tab" aria-controls="introduction" aria-selected="true">Introduction</a>
+                                </li>
+                                <li class="nav-item" data-toggle="collapse" href="#login_list">
+                                    <a class="nav-link" id="" data-toggle="tab" href="#help_login" role="tab" aria-controls="login" aria-selected="false">Login
+
+                                        <ul class="list-unstyled collapse" id="login_list" data-parent="#accordion">
+                                            <a href="#"><li>Login</li></a>
+                                            <a href="#"><li>Remember Me</li></a>
+                                            <a href="#"><li>Forgot Your Password</li></a>
+                                        </ul>
+                                    </a>    
+                                </li>
+                                <li class="nav-item" data-toggle="collapse" href="#homepage_list">
+                                    <a class="nav-link" id="" data-toggle="tab" href="#help_homepage" role="tab" aria-controls="homepage" aria-selected="false">Homepage - Admin
+
+                                        <ul class="list-unstyled collapse" id="homepage_list" data-parent="#accordion">
+                                            <a href="#"><li>Filter by Team</li></a>
+                                            <a href="#"><li>Filter by Staff Name</li></a>
+                                            <a href="#"><li>Filter by Date</li></a>
+                                            <a href="#"><li>Daily</li></a>
+                                            <a href="#"><li>Weekly</li></a>
+                                            <a href="#"><li>Monthly</li></a>
+                                            <a href="#"><li>Edit Time Tracking</li></a>
+                                            <a href="#"><li>Notification</li></a>
+                                            <a href="#"><li>Staff Detail</li></a>
+                                        </ul>
+                                    </a>    
+                                </li>
+                                <li class="nav-item" data-toggle="collapse" href="#profile_list">
+                                    <a class="nav-link" id="" data-toggle="tab" href="#help_profile" role="tab" aria-controls="profile" aria-selected="false">Profile
+
+                                        <ul class="list-unstyled collapse" id="profile_list" data-parent="#accordion">
+                                            <a href="#"><li>My Profile</li></a>
+                                            <a href="#"><li>Bills</li></a>
+                                            <a href="#"><li>Settings</li></a>
+                                            <a href="#"><li>Change Password</li></a>
+                                            <a href="#"><li>Logout</li></a>
+                                        </ul>
+                                    </a>    
+                                </li>
+                                <li class="nav-item" data-toggle="collapse" href="#">
+                                    <a class="nav-link active" id="" data-toggle="tab" href="#help_intro" role="tab" aria-controls="introduction" aria-selected="true">Setting - Staff</a>
+                                </li>
+                                <li class="nav-item" data-toggle="collapse" href="#">
+                                    <a class="nav-link active" id="" data-toggle="tab" href="#help_intro" role="tab" aria-controls="introduction" aria-selected="true">Setting - Task</a>
+                                </li>
+                                <li class="nav-item" data-toggle="collapse" href="#">
+                                    <a class="nav-link active" id="" data-toggle="tab" href="#help_intro" role="tab" aria-controls="introduction" aria-selected="true">Setting - Team</a>
+                                </li>
+                                <li class="nav-item" data-toggle="collapse" href="#">
+                                    <a class="nav-link active" id="" data-toggle="tab" href="#help_intro" role="tab" aria-controls="introduction" aria-selected="true">Setting - Day Off</a>
+                                </li>
+                                <li class="nav-item" data-toggle="collapse" href="#">
+                                    <a class="nav-link active" id="" data-toggle="tab" href="#help_intro" role="tab" aria-controls="introduction" aria-selected="true">Attendance Admin</a>
+                                </li>
+                                <li class="nav-item" data-toggle="collapse" href="#">
+                                    <a class="nav-link active" id="" data-toggle="tab" href="#help_intro" role="tab" aria-controls="introduction" aria-selected="true">Desktop App</a>
+                                </li>
+                                <li class="nav-item" data-toggle="collapse" href="#">
+                                    <a class="nav-link active" id="" data-toggle="tab" href="#help_intro" role="tab" aria-controls="introduction" aria-selected="true">Dashboard - Staff</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-md-8">
+                        <div class="tab-content">
+                            <div class="tab-pane fade show active" id="help_intro" role="tabpanel" aria-labelledby="">
+                                <h2 class="mb-4">Bills</h2>
+                                <p>This page is used to view the subscription and transaction histories which have/have not been completed. This page can also be used to upload payment proof and to view the Invoice and Tax files of a transaction.</p>
+                                <ol class="pl-3">
+                                    <li class="mb-3"><b>Select Year</b>: This filter can be used to view all the transactions which happened in the span of the selected year.</li>
+                                    <li class="mb-3"><b>UNPAID</b>: This table displays paid, unpaid, pending, and overdue bills.</li>
+                                    <li class="mb-3"><b>PAID</b>: This table displays bills which have been paid and confirmed.</li>
+                                    <li class="mb-3"><b>Invoice & Tax</b>: These buttons will only appear after payments have been confirmed. The files will always be saved in the system, and can be downloaded whenever. If there are no buttons, it means that the payment is free.</li>
+                                </ol>
+                            </div>
+                            <div class="tab-pane fade" id="help_login" role="tabpanel" aria-labelledby="">
+                                Login
+                            </div>                        
+                            <div class="tab-pane fade" id="help_homepage" role="tabpanel" aria-labelledby="">
+                                Homepage
+                            </div>                        
+                            <div class="tab-pane fade" id="help_profile" role="tabpanel" aria-labelledby="">
+                                Homepage
+                            </div>                        
+                        </div>
+                        <div class="">
+                            <hr class="mb-3 mt-5">
+                            Do you need more help? Contact Us through these links: <br>
+                            WA: <br>    
+                            Email:
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    </div>
+    
+    <?php include "footer.php" ?>
+	<script src="/js/jquery-3.4.1.min.js"></script>
+    <script src="/js/bootstrap.min.js"></script>
+    <script>
+        var element = document.getElementById("popup-text");
+
+        var t=setTimeout(openPopUp,5000);
+        function openPopUp() {
+        element = document.getElementById("popup-text");
+        element.style.visibility = "visible";
+        element.style.opacity = "1";
+        }
+    </script>
+    <script>
+        $(".help__explain .explain__topic .nav-item[href='#']").click(function(){
+            $(".collapse").collapse('hide')
+        })
+    </script>
+</body>
