eb9dafed013a90b07630f1b403c932444c97ef0b
Bagas Ranggen <bagas@logique.co.id>
2021-04-20 16:05:37 +0700

    add new page: definisi HRIS (en - id)

---
+++ hris-definition-functions.php
@@ -0,0 +1,192 @@
+<?php 
+    $page = array(
+        "id" => "fungsi-aplikasi-hris",
+        "jp" => "hris-info",
+    );
+    $page_title = 'Definition and Functions of the HRIS Application / Software | Dokodemo-Kerja';
+
+    $meta_description = 'HRIS applications and software provide many benefits for a company’s HR Department in today\'s digital era. Check out the full explanation here.';
+    $meta_keyword = 'dokodemo, dokodemo-kerja, Dokodemo kasus, Digital Logique Indonesia, sistem kerja jarak jauh, system kerja remote, perusahan pengembangan system, monitor karyawan, alat kerja remote, alat untuk manajer, perusahaan akuntansi';
+
+    $og_title = 'Definition and Functions of the HRIS Application / Software | Dokodemo-Kerja';
+    $og_image = 'https://dokodemo-kerja.com/img/dokodemo-og.jpg';
+    $og_desription = 'HRIS applications and software provide many benefits for a company’s HR Department in today\'s digital era. Check out the full explanation here.';
+
+    $add_css = '
+    <link rel="stylesheet" href="/css/slick.css" />
+    <link rel="stylesheet" href="/css/slick-theme.css" />
+    <link rel="stylesheet" href="/css/style-home.css" />
+    <link rel="stylesheet" href="/css/style-case-study.css" />';
+
+    include($_SERVER['DOCUMENT_ROOT'].'/component/header.php');
+?>
+
+<div class="case-study home">
+
+    <section class="mb--30px fs-14">
+        <div class="container">
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb breadcrumb--main">
+                    <li class="breadcrumb-item"><a href="/">Home</a></li>
+                    <li class="breadcrumb-item"><a href="/hris-info">HRIS Info</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">Definition and Functions of an HRIS Application</li>
+                </ol>
+            </nav>
+        </div>
+    </section>
+
+    <section class="mb--70px">
+        <div class="color--blue container">
+            <div class="text-center title--bg title--bg-border title--bg-border-right" data-title-bg="DEFINITION">                            
+                <h1 class="mb-0 font-weight-bold fs-34">Definition and Functions of an HRIS Application</h1>
+            </div>
+
+            <div class="fs-14 row justify-content-center">
+                <div class="col-md-10">
+                    <p>HRIS applications are currently the most widely used form of software by Human Resource Departments (HRD). As we know, the collection of employee data plays an important role for any company. That's why HRIS applications are designed to make it easier for management to comprehensively manage such data all within one secure location without having to search through various different file locations.</p>
+                    <p class="mb-0">In order to better understand what the HRIS application is and what it does, we aim to inform you about the definition of HRIS and go through its main functions.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="mb--70px py--30px color--blue bg--l-blue case-study__dokodemo">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-10">
+                    <div class="row">
+                        <div class="col-md-auto mb-4 mb-md-0">
+                            <h2 class="fs-34 font-weight-bold">“Dokodemo-Kerja”</h2>
+                        </div>
+                        <div class="col-md fs-14">
+                            <p><b>Aplikasi Absensi Online</b> terbaik dengan fiturlengkap untuk meningkatkan produktivitaskerja para karyawan. Pengelolaan Absensi, Cuti, hingga Evaluasi hanya melalui satu Aplikasi</p>
+                            <p class="mb-0 font-weight-bold">Bekerja dimana saja dengan Dokodemo-Kerja. Coba sekarang!</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="mb--70px color--blue">
+        <div class="container">
+            
+            <div class="text-center title--bg title--bg-border title--bg-border-left" data-title-bg="HRIS">                            
+                <h2 class="mb-0 font-weight-bold fs-34">What is HRIS (Human Resources Information System)?</h2>
+            </div>
+
+            <div class="fs-14 row justify-content-center">
+                <div class="col-md-10">
+                    <p>HRIS is a system used to collect and store a company’s employee data. HRIS can be used to store, process, and manage a wide variety of such information, ranging from names, addresses, employee IDs, social security, and much more. Overall, HRIS presents a smarter method of storing information into an employee database.</p>
+                    <p>In addition, it is important to know that in general, HRIS systems also have functions that support HR management, such as employee attendance & leave management, performance appraisals, administrative arrangements regarding employee benefits or salaries, and much more. Overall, through HRIS systems, the process of human resource management can be done digitally and effectively.</p>
+                    <p class="mb-0">In using this system, companies utilize cloud-based or on-premise HRIS applications. If the company uses on-premise software, information storage will be based within databases and servers being operated by the company itself. Meanwhile, through the use of cloud-based software, such data will be stored within the HRIS application provider's data center server.</p>
+                </div>
+            </div>
+            
+        </div>
+    </section>
+
+    <section class="mb--70px color--blue">
+        <div class="container">
+            
+            <div class="text-center title--bg title--bg-border title--bg-border-right" data-title-bg="HRIS COMPONENT">                            
+                <h2 class="mb-0 font-weight-bold fs-34">HRIS System Components</h2>
+            </div>
+
+            <div class="fs-14 row justify-content-center">
+                <div class="col-md-10">
+
+                    <h3>1. Database</h3>
+                    <p>HRIS has a database used to store employee information. The HR team can enter the entirety of such stored employee data within the system, being able to access this information from anywhere at any time.</p>
+
+                    <h3>2. Time and labor management</h3>
+                    <p>HRIS utilizes a feature that allows employees to enter their own work hours or to leave requests as they see fit. Through these components, the HRIS application can thereby improve the HR department's ability to track employee timelines and their overall attendance rate.</p>
+
+                    <h3>3. Payroll function</h3>
+                    <p>HR can accurately calculate the number of hours worked so as to determine appropriate employee salary payments amounts.</p>
+
+                    <h3>4. Allowances or benefits</h3>
+                    <p>HRIS accounts for employee assistance arrangements, such as medical benefits or pension fund investments.</p>
+
+                    <h3>5. Interface for employees</h3>
+                    <p>Most HRIS applications provide employees with a level of user access. However, employees are only allowed limited access to databases where they can update their personal information, review pay scales, or apply for leave.</p>
+
+                </div>
+            </div>
+        
+        </div>
+    </section>
+
+    <section class="mb--70px color--blue">
+        <div class="container">
+            
+            <div class="text-center title--bg title--bg-border title--bg-border-left" data-title-bg="HRIS FUNCTION">                            
+                <h2 class="mb-0 font-weight-bold fs-34">Functions of HRIS (Human Resource Information System)</h2>
+            </div>
+
+            <div class="fs-14 row justify-content-center">
+                <div class="col-md-10">
+                    <p>The Human Resource Department plays a very important role within the company. The HRD team serves in making sure a company’s workforce is of the utmost quality, which entails maintaining a high level of employee productivity, among other roles. In order to fully optimize the overall efficiency of the HR team's work processes, it is necessary to have the right HRIS application.</p>
+                    <p>A HRIS application can provide various benefits for your company, such as allowing the HR team to optimize their administrative tasks. By doing so, the HR team can potentially focus their attention on more pressing HR tasks.</p>
+                    <p>Of the various HRIS components previously described, here are some of the main functions of the HRIS application:</p>
+
+                    <div class="pl-5">
+                        <h3>1. Record function</h3>
+                        <p>A HRIS can work as a record system which is able to track any changes related to employee data.</p>
+
+                        <h3>2. Fulfill the completeness of employee data</h3>
+                        <p>Most companies will collect and store comprehensive data about their employees. For example, a company most likely stores a variety of contact information about the individual, in the case of an accident, or in regards to taxes, etc. All information will be stored securely through the HRIS application. That is why HRIS is often relied on as an intelligent source of staffing data.</p>
+
+                        <h3>3. Improve the overall efficiency of the HR administration processes</h3>
+                        <p>Digitally based methods of management can greatly raise the productivity of your company's HR team. Because such systems store a variety of data within a single location, the HR team will undoubtedly find it easier to update, track, and manage vast amounts of employee data.</p>
+
+                        <h3>4. The process of analyzing and evaluating employee performance</h3>
+                        <p>The HRIS application allows you to analyze and review the overall performance of your employees. Most HRIS applications now have an e-attendance feature that automatically records employee attendance hours so as to gauge each employee’s individual level of discipline. In addition, the HRIS application also provides a variety of useful data that can be used to evaluate and report on an employee’s performance.</p>
+
+                        <h3>5. Self-Service for employees</h3>
+                        <p>The HRIS application offers self-service capabilities for its employees. This means that employees can handle various metters independently regarding their data within the company. Examples include the ability to update personal information, view remaining leave quotas, apply for leave, track PTO, and others.</p>
+
+                    </div>
+
+                    <p>Companies that are able to make good use of the HRIS application will get a strong sense of the benefits of doing so. This software is deliberately designed with various features that work to simplify the human resource management process for better efficiency and effectiveness. If your company is growing and currently has a substantial number of employees, then it is time for you to switch from a manually-based system to a digital one.</p>
+                    <p>Dokodemo-Kerja, as one of the best HRIS software or applications in Indonesia, was built to make a positive contribution to the process of improving employee performance and overall professionalism within the workplace.</p>
+
+                </div>
+            </div>
+            
+        </div>
+    </section>
+    
+</div>
+    
+<?php include($_SERVER['DOCUMENT_ROOT'].'/component/demo-banner.php'); ?>
+<?php include($_SERVER['DOCUMENT_ROOT'].'/component/footer.php'); ?>
+
+<script>
+    $(function() {        
+        $(".case-study__slide").slick({
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            arrows: true,
+            responsive: [
+                {
+                    breakpoint: 991,
+                    settings: {
+                        slidesToShow: 2,
+                    }
+                },
+                {
+                    breakpoint: 767,
+                    settings: {
+                        slidesToShow: 1,
+                    }
+                }
+                // You can unslick at a given breakpoint now by adding:
+                // settings: "unslick"
+                // instead of a settings object
+            ]
+
+        })
+    });
+</script>
+<?php include($_SERVER['DOCUMENT_ROOT'].'/component/footer-end.php') ?>
