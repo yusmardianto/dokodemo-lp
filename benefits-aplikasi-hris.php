a50913d91e403354920b22df3e210242cad1d1a8
calvinocta <calvin@logique.co.id>
2021-05-11 15:54:39 +0700

    new content

---
+++ benefits-aplikasi-hris.php
@@ -0,0 +1,126 @@
+<?php
+$page = array(
+    "id" => "manfaat-aplikasi-hris",
+    "jp" => "benefits-aplikasi-hris",
+);
+$page_title = 'Benefits of HRIS Applications for Both the Company & Employee Alike!| Dokodemo-Kerja';
+
+$meta_description = 'There are many benefits a HRIS application can provide for a company’s HR department and its employees. For a full explanation, click here!';
+$meta_keyword = 'dokodemo, dokodemo-kerja, Dokodemo kasus, Digital Logique Indonesia, sistem kerja jarak jauh, system kerja remote, perusahan pengembangan system, monitor karyawan, alat kerja remote, alat untuk manajer, perusahaan akuntansi';
+
+$og_title = 'Benefits of HRIS Applications for Both the Company & Employee Alike!| Dokodemo-Kerja';
+$og_image = 'https://dokodemo-kerja.com/img/dokodemo-og.jpg';
+$og_desription = 'There are many benefits a HRIS application can provide for a company’s HR department and its employees. For a full explanation, click here!';
+
+$add_css = '
+    <link rel="stylesheet" href="/css/slick.css" />
+    <link rel="stylesheet" href="/css/slick-theme.css" />
+    <link rel="stylesheet" href="/css/style-home.css" />
+    <link rel="stylesheet" href="/css/manfaat-aplikasi-hris/index.css" />
+    <link rel="stylesheet" href="/css/style-case-study.css" />';
+
+include($_SERVER['DOCUMENT_ROOT'] . '/component/header.php');
+?>
+
+<div class="case-study home">
+
+    <section class="mb--30px fs-14">
+        <div class="container">
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb breadcrumb--main">
+                    <li class="breadcrumb-item"><a href="/">Home</a></li>
+                    <li class="breadcrumb-item"><a href="/hris-info">HRIS Info</a></li>
+                    <li class="breadcrumb-item active" aria-current="page"> Benefits of HRIS Applications</li>
+                </ol>
+            </nav>
+        </div>
+    </section>
+
+    <section class="mb--70px margintop-80">
+        <div class="color--blue container">
+            <div class="text-center title--bg title--bg-border title--bg-border-left" data-title-bg="BENEFITS">
+                <h1 class="mb-0 font-weight-bold fs-34"> Benefits of HRIS Applications for Both the Company & Employee Alike</h1>
+            </div>
+
+            <div class="fs-14 row justify-content-center">
+                <div class="col-md-10">
+                    <p>HRIS applications provide a variety of benefits for both the company as well as those within its employ. In terms of function, The HRIS application is a system used to collect and systematically store vast amounts of employee data. They can also be categorized as intelligent employee information databases, offering a wide variety of solutions in regards to efficiently managing employee records.</p>
+                    <p>In this article, we seek to provide an explanation as to the overall benefits of using the HRIS application, both for company and the employee alike.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="mb--70px color--blue margintop-150">
+        <div class="container">
+
+            <div class="text-center title--bg title--bg-border title--bg-border-right" data-title-bg="COMPANIES">
+                <h2 class="mb-0 font-weight-bold fs-34">Benefits of the HRIS Applications for Companies</h2>
+            </div>
+
+            <div class="fs-14 row justify-content-center">
+                <div class="col-md-10">
+                    <ol class="list-item">
+                        <li class="mt-3">
+                            <h3 class="list-manfaat-hris">Providing a well-organized database</h3>
+                            <p>Manually organizing a company’s employee data can be very difficult, simply because this requires the use of a lot of paper documents. In this case, the HR team needs to print said documents out, physically organize them and then file them somewhere safe such as a cabinet. Afterwards, when a specific document is required, it might take a while to find it, as employees would have to search through a massive assortment of files.</p>
+                            <p>Such disorder simply would not happen through the use of the HRIS application. because data can be collected, tracked, updated and searched digitally. Employee data will be stored neatly in the system so that data management can be carried out in a more organized manner.</p>
+                        </li>
+                        <li class="mt-3">
+                            <h3 class="list-manfaat-hris">Helps in reducing the administrative burden of the HR team</h3>
+                            <p>A HRIS application offers the ability to automate repetitive tasks such as managing leave and absences, monitoring employee performances, payroll, and others. Through this function, the HR team's administrative workload can be reduced so that the team can focus on doing other important tasks.</p>
+                        </li>
+                        <li class="mt-3">
+                            <h3 class="list-manfaat-hris">Saves a lot of time</h3>
+                            <p>When a company uses a HRIS application, the HR team or management can save a lot of time because the HRIS application can help simplify processes and automate tasks. In addition, the HRIS application also contains a database that is mutually integrated and can act as a place that stores various articles of information that serve to support the overall HR activities taking place within the company. Thus management can save a lot of time when searching for employee data or when they want to share information across their employee base.</p>
+                        </li>
+                        <li class="mt-3">
+                            <h3 class="list-manfaat-hris">Helps with the decision making process</h3>
+                            <p>Companies usually require further insight and a collection of various pieces of data before making decisions such as labor arrangements, compensation, or in regards to a vast swath of other things. Through the use of a HRIS application, companies can easily gain useful insights about their employees, thereby helping management make the right decisions.</p>
+                        </li>
+
+                        <li>
+                            <h3 class="list-sistem-hris">Improves employee performance and productivity</h3>
+                            <p>Most HRIS applications come with a feature that helps manage attendance (e-attendance) or a feature that records employee attendance hours. This feature can be used by the HR team or company management in order to review employee performances in order to remain productive and disciplined while engaged in work.</p>
+                        </li>
+                    </ol>
+                </div>
+            </div>
+
+        </div>
+    </section>
+
+    <section class="mb--70px color--blue margintop-150">
+        <div class="container">
+
+            <div class="text-center title--bg title--bg-border title--bg-border-right" data-title-bg="EMPLOYEES">
+                <h2 class="mb-0 font-weight-bold fs-34">Benefits of HRIS Applications for Employees</h2>
+            </div>
+
+            <div class="fs-14 row justify-content-center">
+                <div class="col-md-10">
+                    <ol class="list-item">
+                        <li class="mt-3">
+                            <h3 class="list-manfaat-hris">Allows for employee self-service</h3>
+                            <p>Through the HRIS application, employees can independently handle various things in relation to their data. Employees can update their personal information, manage leave, track PTOs, and much more without having to contact the HR team.</p>
+                        </li>
+                        <li class="mt-3">
+                            <h3 class="list-manfaat-hris">Employees can individually retrieve required information</h3>
+                            <p>Because data is provided in a transparent manner, employees no longer need to contact the HR team to receive the information they require. Although it is currently the case that most HRIS systems provide access for employees, access is limited to certain areas of information, such as databases for updating personal information, requests for leave, or salary scales.</p>
+                            <p>The HRIS application was developed and designed to streamline complex employee management processes. Both companies and employees will be greatly helped by the presence of this application.</p>
+                            <p>If your company incorporates a large number of employees, then it is time for you to switch to using a digital platform for an easier and simpler process of employee data management. Dokodemo-Kerja can be a great solution for you.</p>
+                        </li>
+                    </ol>
+                </div>
+            </div>
+
+        </div>
+    </section>
+
+</div>
+
+<?php include($_SERVER['DOCUMENT_ROOT'] . '/component/demo-banner.php'); ?>
+<?php include($_SERVER['DOCUMENT_ROOT'] . '/component/footer.php'); ?>
+<script>
+</script>
+<?php include($_SERVER['DOCUMENT_ROOT'] . '/component/footer-end.php') ?>
---
+++ hris-hcm-hrms-technology.php
@@ -0,0 +1,175 @@
+<?php
+$page = array(
+    "id" => "teknologi-hris-hcm-hrms",
+    "jp" => "hris-hcm-hrms-technology",
+);
+$page_title = 'Perbedaan Teknologi HRIS, HCM, dan HRMS | Dokodemo-Kerja';
+
+$meta_description = 'Teknologi HRIS, HCM, dan HRMS memiliki fitur dan karakteristik masing-masing. Berikut kami sediakan hal-hal mendasar yang membedakan ketiga teknologi tersebut.
+
+';
+$meta_keyword = 'dokodemo, dokodemo-kerja, Dokodemo kasus, Digital Logique Indonesia, sistem kerja jarak jauh, system kerja remote, perusahan pengembangan system, monitor karyawan, alat kerja remote, alat untuk manajer, perusahaan akuntansi';
+
+$og_title = 'Perbedaan Teknologi HRIS, HCM, dan HRMS | Dokodemo-Kerja';
+$og_image = 'https://dokodemo-kerja.com/img/dokodemo-og.jpg';
+$og_desription = 'Teknologi HRIS, HCM, dan HRMS memiliki fitur dan karakteristik masing-masing. Berikut kami sediakan hal-hal mendasar yang membedakan ketiga teknologi tersebut.';
+
+$add_css = '
+    <link rel="stylesheet" href="/css/slick.css" />
+    <link rel="stylesheet" href="/css/slick-theme.css" />
+    <link rel="stylesheet" href="/css/style-home.css" />
+    <link rel="stylesheet" href="/css/teknologi-hris/index.css" />
+    <link rel="stylesheet" href="/css/style-case-study.css" />';
+
+include($_SERVER['DOCUMENT_ROOT'] . '/component/header.php');
+?>
+
+<div class="case-study home">
+
+    <section class="mb--30px fs-14">
+        <div class="container">
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb breadcrumb--main">
+                    <li class="breadcrumb-item"><a href="/">Home</a></li>
+                    <li class="breadcrumb-item"><a href="/hris-info">HRIS Info</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">Perbedaan Teknologi HRIS, HCM, dan HRMS</li>
+                </ol>
+            </nav>
+        </div>
+    </section>
+
+    <section class="mb--70px margintop-80">
+        <div class="color--blue container">
+            <div class="text-center title--bg title--bg-border title--bg-border-left" data-title-bg="PERBEDAAN">
+                <h1 class="mb-0 font-weight-bold fs-34">Perbedaan Mendasar antara Teknologi HRIS, HCM, dan HRMS</h1>
+            </div>
+
+            <div class="fs-14 row justify-content-center">
+                <div class="col-md-10">
+                    <p>Secara garis besar, HR software terbagi ke dalam 3 jenis teknologi yang berbeda yaitu teknologi HRIS, HCM, dan HRMS. Masing-masing kategori tersebut memiliki karakteristik dan kemampuan yang berbeda sehingga penting bagi perusahaan untuk mengetahui apa perbedaan dari ketiga teknologi tersebut sebelum memutuskan untuk menggunakannya.</p>
+                    <p>Dengan memiliki pemahaman yang baik mengenai teknologi HRIS, HCM, dan HRMS maka Anda dapat memilih solusi yang tepat dan sesuai dengan apa yang dibutuhkan oleh perusahaan Anda.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="mb--70px margintop-80">
+        <div class="color--blue container">
+            <div class="text-center title--bg title--bg-border title--bg-border-right" data-title-bg="HRIS">
+                <h2 class="mb-0 font-weight-bold fs-34">Teknologi HRIS</h2>
+            </div>
+
+            <div class="fs-14 row justify-content-center">
+                <div class="col-md-10">
+                    <p>Teknologi HRIS atau Human Resources Information System adalah perangkat lunak yang memungkinkan penggunanya untuk mengelola, melacak, dan mengotomatiskan kebutuhan inti SDM secara digital.</p>
+                    <p>Platform HRIS biasanya dipergunakan sebagai pusat database mengenai informasi karyawan. Teknologi HRIS dapat menghubungkan data ke masing-masing karyawan, departemen, cabang kantor, ataupun bagian lain di dalam perusahaan sehingga pengelolaan data dapat dilakukan pada satu tempat dengan cara yang lebih mudah. Selain itu, teknologi HRIS juga menawarkan kemampuan untuk melakukan pelaporan atau analitik pada tingkat dasar.</p>
+                    <p>Jadi, ketika Anda menemukan sebuah HR software dengan beberapa kemampuan berikut, maka dapat dikatakan bahwa software tersebut menggunakan teknologi HRIS:</p>
+                    <ol class="list-item">
+                        <li class="mt-3">
+                            <span class="list-item-title">Mengumpulkan data karyawan</span>
+                            <p>HRIS mengumpulkan data karyawan secara digital yang berisi informasi mengenai profil karyawan, kontak, detail informasi terkait hubungan kerja dengan perusahaan, dan masih banyak lagi. </p>
+                        </li>
+                        <li class="mt-3">
+                            <span class="list-item-title">Payroll</span>
+                            <p>HRIS memiliki fungsi yang berkaitan dengan perhitungan gaji karyawan baik itu setoran gaji karyawan maupun pemotongan jumlah gaji.</p>
+                        </li>
+                        <li class="mt-3">
+                            <span class="list-item-title">Benefits Management</span>
+                            <p>Teknologi HRIS dapat memudahkan pengelolaan benefit yang diberikan oleh perusahaan untuk karyawan seperti asuransi, dana pensiun, atau yang lain.</p>
+                        </li>
+                        <li class="mt-3">
+                            <span class="list-item-title">Rekrutmen</span>
+                            <p>HRIS dapat melakukan otomatisasi perekrutan mendasar seperti memposting pekerjaan, melacak kandidat, serta mengelola lamaran online yang diterima. </p>
+                        </li>
+                        <li class="mt-3">
+                            <span class="list-item-title">Mencatat waktu kehadiran karyawan</span>
+                            <p>HRIS dapat digunakan untuk mengawasi jam kerja karyawan, absensi, ataupun cuti. </p>
+                        </li>
+                        <li class="mt-3">
+                            <span class="list-item-title">Self service</span>
+                            <p>HRIS memberikan karyawan akses ke database serta fungsionalitas untuk memperbarui info pribadi secara mandiri.</p>
+                        </li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="mb--70px margintop-80">
+        <div class="color--blue container">
+            <div class="text-center title--bg title--bg-border title--bg-border-left" data-title-bg="HCM">
+                <h2 class="mb-0 font-weight-bold fs-34">Teknologi HCM</h2>
+            </div>
+
+            <div class="fs-14 row justify-content-center">
+                <div class="col-md-10">
+                    <p>Teknologi HCM atau Human Capital Management adalah teknologi yang lebih komprehensif daripada HRIS. HCM menawarkan kemampuan yang dimiliki oleh teknologi HRIS dan menambahkan kemampuan lain yaitu untuk talent management. Jadi, ketika Anda menggunakan teknologi ini, maka Anda dapat melihat kemampuan dalam perencanaan karir, pembelajaran dan pengembangan, dan lain-lain.</p>
+                    <p>Berikut adalah beberapa kemampuan utama dari teknologi HCM yang mungkin berguna untuk perusahaan Anda:</p>
+                    <ol class="list-item">
+                        <li class="mt-3">
+                            <span class="list-item-title">Semua fitur HRIS</span>
+                        </li>
+                        <li class="mt-3">
+                            <span class="list-item-title">Fungsi Administratif HR</span>
+                            <p>HCM dapat dipergunakan untuk pengelolaan data administratif perusahaan seperti data karyawan, tunjangan, penggajian, dan lain-lain.</p>
+                        </li>
+                        <li class="mt-3">
+                            <span class="list-item-title">Benefits Management</span>
+                            <p>HR service delivery mengacu pada fungsi yang dapat mendukung dan memberikan layanan kepada karyawan. Contohnya seperti akses ke panduan prosedur dan kebijakan, manajemen dokumen, atau info mendasar tentang perusahaan.</p>
+                        </li>
+                        <li class="mt-3">
+                            <span class="list-item-title">Talent Management</span>
+                            <p>Teknologi HCM dipergunakan untuk talent management atau pengembangan SDM seperti membantu proses perekrutan, training, pembelajaran dan pengembangan skill, pelacakan kinerja, job posting, dan lain-lain. </p>
+                        </li>
+                        <li class="mt-3">
+                            <span class="list-item-title">Workforce management</span>
+                            <p>Teknologi HCM dapat digunakan untuk mencatat <a href="/ind">absensi karyawan</a>, manajemen kehadiran, serta pembuatan jadwal.</p>
+                        </li>
+                        <li class="mt-3">
+                            <span class="list-item-title">Kemampuan Global HR</span>
+                            <p>Fitur multi-currency dan multilingual untuk support kebutuhan internasional. </p>
+                        </li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="mb--70px margintop-80">
+        <div class="color--blue container">
+            <div class="text-center title--bg title--bg-border title--bg-border-right" data-title-bg="HRMS">
+                <h2 class="mb-0 font-weight-bold fs-34">Teknologi HRMS</h2>
+            </div>
+
+            <div class="fs-14 row justify-content-center">
+                <div class="col-md-10">
+                    <p>Teknologi HRMS atau Human Resource Management System adalah teknologi yang mencakup semua fitur yang ditawarkan teknologi HRIS dan HCM dengan tambahan fungsi payroll dan TLM (Time and Labour Management). Tidak heran, jika sebagian orang beranggapan bahwa HRMS adalah teknologi paling komprehensif untuk solusi software HR.</p>
+                    <p>Teknologi HRMS pada umumnya akan menyediakan beberapa kemampuan, yaitu:</p>
+                    <ol class="list-item">
+                        <li class="mt-3">
+                            <span class="list-item-title">Fitur-fitur umum dari teknologi HRIS dan HCM</span>
+                        </li>
+                        <li class="mt-3">
+                            <span class="list-item-title">Payroll</span>
+                            <p>Kemampuan untuk menghitung pembayaran, pemotongan pajak, dan lain-lain.</p>
+                        </li>
+                        <li class="mt-3">
+                            <span class="list-item-title">Time and Labor Management (TLM)</span>
+                            <p>Kemampuan untuk mengontrol jadwal karyawan, melacak waktu proyek, dan kemampuan lain untuk meningkatkan produktivitas serta profitabilitas karyawan dan perusahaan.</p>
+                        </li>
+                    </ol>
+                    <p>Dengan adanya teknologi HRIS, HCM, dan HRMS, saat ini pengelolaan karyawan dapat dilakukan secara lebih efektif dan efisien. Anda tidak lagi perlu meminta karyawan untuk terus mencetak dokumen mengenai data-data mereka karena semua informasi dapat tersimpan secara digital di dalam sistem.</p>
+                    <p>Ketika memilih software HR, Anda harus menyesuaikannya dengan kebutuhan di perusahaan Anda. Seperti, seberapa banyak jumlah karyawan yang dikelola, fitur-fitur inti apa saja yang dibutuhkan, anggaran yang dipersiapkan, apakah perangkat mudah untuk dioperasikan, dan lain-lain. Dengan mengevaluasi apa yang menjadi kebutuhan perusahaan, Anda dapat menemukan software HR terbaik untuk perusahaan Anda.</p>
+                    <p>Salah satu software HR yang bisa Anda andalkan adalah Dokodemo-Kerja. Software HR ini memiliki banyak fitur yang mampu menawarkan pengelolaan sistem kerja yang fleksibel tanpa mengesampingkan produktivitas karyawan Anda. Cek fitur Dokodemo-Kerja selengkapnya <a href="/ind/fitur">di sini</a>!</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+</div>
+
+<?php include($_SERVER['DOCUMENT_ROOT'] . '/component/demo-banner.php'); ?>
+<?php include($_SERVER['DOCUMENT_ROOT'] . '/component/footer.php'); ?>
+<script>
+</script>
+<?php include($_SERVER['DOCUMENT_ROOT'] . '/component/footer-end.php') ?>
