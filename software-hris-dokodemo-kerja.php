aeb2782a917a3c48a6126ac93caa47aff95667f5
calvinocta <calvin@logique.co.id>
2021-05-24 10:38:38 +0700

    update software hris en version

--- software-hris-dokodemo-kerja.php
+++ software-hris-dokodemo-kerja.php
@@ -3,16 +3,14 @@ $page = array(
     "id" => "software-hris-dokodemo-kerja",
     "jp" => "software-hris-dokodemo-kerja",
 );
-$page_title = 'Kelebihan Software HRIS Dokodemo-Kerja | Dokodemo-Kerja';
+$page_title = 'Advantages of the HRIS Software “Dokodemo-Kerja” | Dokodemo-Kerja';
 
-$meta_description = 'Dokodemo-Kerja adalah software HRIS yang sudah banyak digunakan oleh berbagai jenis perusahaan di Indonesia. Berikut beberapa kelebihan Dokodemo-kerja.
-
-';
+$meta_description = 'Dokodemo-Kerja is a HRIS software widely used by various companies within Indonesia. Here are some of the advantages of using Dokodemo-Kerja.';
 $meta_keyword = 'dokodemo, dokodemo-kerja, Dokodemo kasus, Digital Logique Indonesia, sistem kerja jarak jauh, system kerja remote, perusahan pengembangan system, monitor karyawan, alat kerja remote, alat untuk manajer, perusahaan akuntansi';
 
-$og_title = 'Kelebihan Software HRIS Dokodemo-Kerja | Dokodemo-Kerja';
+$og_title = 'Advantages of the HRIS Software “Dokodemo-Kerja” | Dokodemo-Kerja';
 $og_image = 'https://dokodemo-kerja.com/img/dokodemo-og.jpg';
-$og_desription = 'Dokodemo-Kerja adalah software HRIS yang sudah banyak digunakan oleh berbagai jenis perusahaan di Indonesia. Berikut beberapa kelebihan Dokodemo-kerja.';
+$og_desription = 'Dokodemo-Kerja is a HRIS software widely used by various companies within Indonesia. Here are some of the advantages of using Dokodemo-Kerja.';
 
 $add_css = '
     <link rel="stylesheet" href="/css/slick.css" />
@@ -32,7 +30,7 @@ include($_SERVER['DOCUMENT_ROOT'] . '/component/header.php');
                 <ol class="breadcrumb breadcrumb--main">
                     <li class="breadcrumb-item"><a href="/">Home</a></li>
                     <li class="breadcrumb-item"><a href="/hris-info">HRIS Info</a></li>
-                    <li class="breadcrumb-item active" aria-current="page">Software HRIS Dokodemo-Kerja</li>
+                    <li class="breadcrumb-item active" aria-current="page">Dokodemo-Kerja HRIS Software</li>
                 </ol>
             </nav>
         </div>
@@ -41,14 +39,14 @@ include($_SERVER['DOCUMENT_ROOT'] . '/component/header.php');
     <section class="mb--70px margintop-80">
         <div class="color--blue container">
             <div class="text-center title--bg title--bg-border title--bg-border-left" data-title-bg="DOKODEMO KERJA">
-                <h1 class="mb-0 font-weight-bold fs-34">Dokodemo-Kerja, Software HRIS untuk Meningkatkan Produktivitas Karyawan</h1>
+                <h1 class="mb-0 font-weight-bold fs-34">Dokodemo-Kerja: HRIS Software Made for Employee Productivity</h1>
             </div>
 
             <div class="fs-14 row justify-content-center">
                 <div class="col-md-10">
-                    <p>Dokodemo-Kerja adalah software HRIS (Human Resource Information System) yang sudah banyak digunakan oleh berbagai jenis perusahaan di Indonesia. Dokodemo-Kerja menyediakan beragam fitur yang berguna sehingga pengelolaan karyawan dapat dilakukan secara lebih efektif dan efisien.</p>
-                    <p>Perlu Anda ketahui, ketika memilih software HRIS setidaknya ada beberapa hal yang perlu Anda pertimbangkan seperti jumlah karyawan di perusahaan, kemampuan sistem HRIS, serta ketersediaan layanan training dan support yang berkelanjutan. Oleh karena itu, sebelum menentukan pilihan, ada baiknya jika Anda mengevaluasi apa yang menjadi kebutuhan perusahaan dan mencari fitur-fitur dari software HRIS yang mampu memenuhi kebutuhan tersebut.</p>
-                    <p>Untuk memastikan bahwa Dokodemo-Kerja adalah software HRIS yang tepat untuk perusahaan Anda. Berikut kami informasikan fungsionalitas yang disediakan untuk memudahkan pengelolaan karyawan di perusahaan Anda. </p>
+                    <p>Dokodemo-Kerja is an example of a Human Resource Information System (HRIS) software that has been widely adopted by a variety of companies within Indonesia. Dokodemo-Kerja provides a variety of useful features that assist HR in carrying out employee management more effectively and efficiently.</p>
+                    <p>When choosing an appropriate HRIS to implement within your company, there are a number of things that need to be considered, such as how many people are currently employed by the company, the specific capabilities of the HRIS software, and the availability of ongoing training and support services. Therefore, before making a choice, it is better to evaluate what the company needs first, and then afterwards look for the specific features offered by a HRIS that will be able to meet those requirements.</p>
+                    <p>To ensure that Dokodemo-Kerja is the right HRIS software for your company, we aim to first familiarize you with the overall functions provided by the tool in its capacity to potentially assist employee management in your company:</p>
                 </div>
             </div>
         </div>
@@ -57,13 +55,13 @@ include($_SERVER['DOCUMENT_ROOT'] . '/component/header.php');
     <section class="mb--70px margintop-80">
         <div class="color--blue container">
             <div class="text-center title--bg title--bg-border title--bg-border-right" data-title-bg="HRIS">
-                <h2 class="mb-0 font-weight-bold fs-34">Apa Itu Software HRIS?</h2>
+                <h2 class="mb-0 font-weight-bold fs-34">What is HRIS Software?</h2>
             </div>
 
             <div class="fs-14 row justify-content-center">
                 <div class="col-md-10">
-                    <p>Secara garis besar, software HRIS adalah perangkat lunak yang memungkinkan penggunanya untuk mengelola, melacak, dan mengotomatiskan kebutuhan inti SDM secara elektronik. Software atau aplikasi HRIS biasanya menyediakan database yang saling terintegrasi dan bertindak sebagai suatu tempat yang berisi berbagai informasi mengenai karyawannya. Dengan adanya software HRIS ini maka data-data yang tersimpan di dalam sistem dapat dipergunakan sewaktu waktu untuk menunjang kegiatan HR di suatu perusahaan.</p>
-                    <p>Di Indonesia sendiri, teknologi ini sudah semaikin berkembang dan salah satu software HRIS yang dapat Anda andalkan adalah Dokodemo-Kerja</p>
+                    <p>Broadly speaking, HRIS software is a type of system that allows users to manage, track and automate their company’s core HR needs through electronic means. HRIS software or applications usually provide an integrated database, acting as a storage containing various articles of information about its employees. Through such software, any data stored within the system can be used at any time, thereby supporting the convenience of your company’s HR activities.</p>
+                    <p>In Indonesia itself, this type of technology is increasingly becoming developed. An example of one of the more reliable and advanced types of HRIS software in circulation currently is Dokodemo-Kerja.</p>
                 </div>
             </div>
         </div>
@@ -71,36 +69,34 @@ include($_SERVER['DOCUMENT_ROOT'] . '/component/header.php');
 
     <section class="mb--70px margintop-80">
         <div class="color--blue container">
-            <div class="text-center title--bg title--bg-border title--bg-border-left" data-title-bg="KELEBIHAN">
-                <h2 class="mb-0 font-weight-bold fs-34">Kelebihan Software HRIS Dokodemo-Kerja</h2>
+            <div class="text-center title--bg title--bg-border title--bg-border-left" data-title-bg="ADVANTAGES">
+                <h2 class="mb-0 font-weight-bold fs-34">Advantages provided by the HRIS “Dokodemo-Kerja”</h2>
             </div>
 
             <div class="fs-14 row justify-content-center">
                 <div class="col-md-10">
-                    <p>Dokodemo-Kerja adalah software HRIS yang secara khusus dirancang dan dikembangkan untuk menunjang aktivitas Human Resource. Selain itu, dengan penggunaan Dokodemo, diharapkan perusahaan-perusahaan yang menggunakannya dapat memperoleh berbagai manfaat seperti meningkatkan produktivitas karyawan, mempromosikan sistem kerja yang fleksibel, serta membantu penilaian kinerja karyawan menjadi lebih objektif</p>
-                    <p>Beberapa fitur unggulan dari Software HRIS Dokodemo-Kerja adalah</p>
+                    <p>Dokodemo-Kerja is a HRIS software specifically designed to support Human Resource activities. In addition, with the use of Dokodemo comes the hope that the companies that use it will enjoy the likely benefits of increased employee productivity, the use of a more flexible work system, and the ability to help employee performance appraisals be a more fair and objective process. </p>
+                    <p>Some of the features utilized by Dokodemo-Kerja are as follows:</p>
                     <ol class="list-item">
                         <li class="mt-3">
-                            <h3 class="list-item-title">Fitur absensi dan manajemen kehadiran</h3>
-                            <p>Dokodemo-Kerja dapat berfungsi sebagai aplikasi absensi online (e-absensi). Anda dapat mempergunakan fitur ini untuk melacak dan mengetahui kapan karyawan mulai dan berhenti bekerja.</p>
-                            <p>Selain itu, Dokodemo-kerja juga sangat sesuai apabila Anda memiliki karyawan yang bekerja dengan sistem remote. Karena alat absensi ini disajikan dalam bentuk software, maka karyawan dapat menginstal Dokodemo-Kerja di perangkatnya dan melakukan absensi secara real time tanpa harus datang ke kantor.</p>
-                            <p>Fitur ini akan mengumpulkan data kehadiran karyawan serta total akumulasi jam kerja karyawan selama periode tertentu. Dengan Dokodemo-kerja, Anda juga dapat melihat jumlah hari libur, cuti, sakit, ataupun day off lain yang diajukan oleh karyawan.</p>
+                            <h3 class="list-item-title">Attendance and attendance management features</h3>
+                            <p>Dokodemo-Kerja comes with online attendance (e-absensi) features. You can use this to track and find out when employees have started and stopped working.</p>
+                            <p>Apart from that, Dokodemo-Kerja is very compatible with employees who are required to work through remote systems. Further, because Dokodemo-Kerja functions also as an attendance tool that is presented in software form, employees can therefore install Dokodemo-Kerja on their devices and have their attendance recorded in real time without having to come to the office. </p>
+                            <p>This feature collects employee attendance information as well as records the total accumulated employee working hours during a certain period. With Dokodemo-Kerja, one can also see the number of leaves, sick days, or other types of days off proposed by employees, who can further propose such days off via the application as well.</p>
                         </li>
                         <li class="mt-3">
-                            <h3 class="list-item-title">Self-Service untuk karyawan</h3>
-                            <p>Dokodemo-kerja memberikan akses pada karyawan untuk dapat menangani berbagai hal secara mandiri terkait data-data mereka di perusahaan. Kemampuan self-service yang disediakan oleh Dokodemo-Kerja adalah fitur untuk mengajukan cuti dan day off, melihat total jam kerja yang yang sudah dijalani karyawan, melihat jumlah cuti yang tersisa dalam jangka satu tahun, serta  pendelegasian tugas ketika karyawan cuti.</p>
+                            <h3 class="list-item-title">Self-Service for employees</h3>
+                            <p>Dokodemo-Kerja provides employees with independent access to a variety of things regarding their own data kept in the company. The self-service features provided by Dokodemo-Kerja includes the ability to apply for leaves and days off, view one’s total working hours for any particular day, see the amount of leave days that one has left, and delegate tasks when employees are on leave.</p>
                         </li>
                         <li class="mt-3">
-                            <h3 class="list-item-title">Personnel tracking atau pelacakan karyawan</h3>
-                            <p>Untuk dapat memastikan bahwa karyawan bekerja dengan produktif, Dokodemo-Kera menyediakan fungsionalitas untuk melacak karyawan Anda.</p>
-                            <p>Ketika menggunakan software HR ini, sistem dapat mengumpulkan dan mengambil screenshot layar penggunanya dalam waktu yang acak. Fitur ini sengaja kami sediakan agar tim HR dapat lebih mudah untuk mengawasi karyawan-karyawannya. Meskipun demikian, untuk menjaga privasi pengguna, hasil screenshot akan kami kumpulkan dengan resolusi gambar yang rendah sehingga teks pada email ataupun alat komunikasi lain tidak akan bisa terbaca. </p>
-                            <p>Selain dapat mengambil screenshot secara acak, kemampuan monitoring karyawan juga kami hadirkan dengan menyediakan fitur GPS. Aplikasi ini dapat memantau karyawan Anda yang bekerja secara mobile seperti tim marketing. Dengan menginstal Dokodemo-Kerja di perangkat mobile, maka aktivitas karyawan yang bekerja di luar lingkungan kantor akan tetap dapat terpantau dengan baik.</p>
+                            <h3 class="list-item-title">Personnel tracking or employee tracking</h3>
+                            <p>To ensure that employees work productively, Dokodemo-Kera provides an efficient tracking feature. </p>
+                            <p>When using this HR software, the system collects screenshots of its users during random intervals of their work hours. This feature is provided deliberately so that the company and HR team can more easily monitor those under its employ. However, so as to maintain a degree of user privacy as well, the screenshots collected have a low image resolution so that the text within emails or other communication tools cannot be read.</p>
+                            <p>Besides being able to take screenshots at random, we also provide employee monitoring capabilities through a GPS feature. This application can thus monitor your employees working on a mobile basis, such as your marketing team, for example. By installing Dokodemo-Kerja on one’s mobile device, the activities of employees working away from the office will be thoroughly monitored.</p>
                         </li>
                         <li class="mt-3">
-                            <h3 class="list-item-title">Laporan dan analisa</h3>
-                            <p>Data-data yang disediakan oleh Dokodemo-kerja akn tersedia dalam bentuk laporan sederhana yang mudah dianalisa. Perusahaan ataupun tim HR akan memperoleh laporan mengenai riwayat absensi karyawan, screenshot layar, serta hasil pelacakan GPS. Dengan report tersebut, tim HR dapat menganalisa bagaimana kinerja dari para karyawannya. Fitur ini juga akan memudahkan tim HR untuk menjaga kedisiplinan dan produktivitas karyawan meskipun mereka bekerja dari jarak jauh sekalipun (remote working).</p>
-                            <p>Dengan beragam fitur yang disediakan, Dokodemo-kerja dapat menjadi software HRIS yang Anda andalkan untuk pengelolaan data karyawan. Dokodemo-kerja juga memberikan support yang berkelanjutan dan akan memastikan bahwa Anda dapat menggunakan dan memperoleh manfaat dari penggunaannya. </p>
-                            <p>Dengan beragam fitur yang disediakan, Dokodemo-Kerja dapat menjadi software HRIS yang Anda andalkan. Dokodemo-Kerja juga memberikan support berkelanjutan untuk memastikan agar para pengguna dapat mengoperasikannya dan memperoleh manfaat dari berbagai fungsionalitas yang disediakan.</p>
+                            <h3 class="list-item-title">Reports and analysis</h3>
+                            <p>The data provided by Dokodemo-Kerja is available in the form of a simple and easy to read report. The company or HR team are able to analyze reports based on an employee’s attendance history, desktop screenshots, and GPS tracking results. Through such reports, the HR team can thus analyze the overall performances of its employees. This feature will also make it easier for the HR team to maintain a level of discipline and productivity, regardless of their remote work status. Through the variety of features provided by Dokodemo-Kerja, this HRIS software is one that can be thoroughly relied upon for one’s employee data management needs. </p>
                         </li>
                     </ol>
                 </div>
