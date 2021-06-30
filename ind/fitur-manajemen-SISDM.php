e3460364dcd82f6b52ff78db6440807ef6696b15
21fahmi <fahmi@logique.co.id>
2021-06-30 12:09:26 +0700

    create new page fitur manajemen SISDM *en *id

---
+++ ind/fitur-manajemen-SISDM.php
@@ -0,0 +1,187 @@
+<?php
+$page = array(
+    "en" => "types-of-hris",
+    "jp" => "types-of-hris",
+);
+$page_title = 'Manajemen Sistem Informasi SDM | Dokodemo-Kerja';
+
+$meta_description = 'SISDM atau juga dikenal dengan HRIS memiliki beragam fitur yang berguna untuk merampingkan proses manajemen SDM yang rumit. Berikut adalah fitur-fitur tersebut.';
+$meta_keyword = 'dokodemo, dokodemo-kerja, Dokodemo kasus, Digital Logique Indonesia, sistem kerja jarak jauh, system kerja remote, perusahan pengembangan system, monitor karyawan, alat kerja remote, alat untuk manajer, perusahaan akuntansi';
+
+$og_title = 'Manajemen Sistem Informasi SDM | Dokodemo-Kerja';
+$og_image = 'https://dokodemo-kerja.com/ind/fitur-manajemen-SISDM';
+$og_desription = 'Terdapat beberapa jenis software HRIS yang dapat dipergunakan untuk merampingkan proses manajemen SDM di perusahaan Anda. Simak penjelasan selengkapnya di sini!';
+
+$add_css = '
+    <link rel="stylesheet" href="/css/slick.css" />
+    <link rel="stylesheet" href="/css/slick-theme.css" />
+    <link rel="stylesheet" href="/css/style-home.css" />
+    <link rel="stylesheet" href="/css/jenis-software-hris/index.css" />
+    <link rel="stylesheet" href="/css/style-case-study.css" />';
+
+include($_SERVER['DOCUMENT_ROOT'] . '/ind/component/header.php');
+?>
+
+<div class="case-study home">
+    <section class="mb--30px fs-14">
+        <div class="container">
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb breadcrumb--main">
+                    <li class="breadcrumb-item"><a href="/ind/">Beranda</a></li>
+                    <li class="breadcrumb-item"><a href="/ind/info-hris">Info HRIS</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">Fitur-fitur SISDM</li>
+                </ol>
+            </nav>
+        </div>
+    </section>
+
+    <section class="margintop-80">
+        <div class="color--blue container">
+            <div class="text-center title--bg title--bg-border title--bg-border-right" data-title-bg="MENGENAL SISDM">
+                <h2 class="mb-0 font-weight-bold fs-34">Mengenal Fitur SISDM, Sistem Cerdas untuk Merampingkan Proses Manajemen SDM</h2>
+            </div>
+
+            <div class="fs-14 row justify-content-center">
+                <div class="col-md-10">
+                    <p class="text-base">Sistem Informasi Sumber Daya Manusia atau SISDM merupakan salah satu jenis perangkat lunak yang dibutuhkan oleh divisi SDM. Perangkat lunak ini menyediakan berbagai fitur yang berguna untuk proses manajemen SDM seperti mengelola dan memantau kinerja karyawan secara ringkas dan sederhana.</p>
+                    <p class="text-base">Divisi SDM atau Sumber Daya Manusia memiliki peran penting di dalam sebuah perusahaan. Meskipun mereka tidak berhubungan langsung dengan profit perusahaan, mereka memiliki tanggung jawab penting untuk mengelola orang-orang / karyawan agar bisa menjalankan masing-masing perannya agar perusahaan dapat meraih tujuan yang ingin dicapai.</p>
+                    <p class="text-base mb-0">Divisi SDM tentu akan membutuhkan banyak tenaga dan waktu jika sebuah perusahaan memiliki ratusan karyawan. Untungnya, seiring dengan perkembangan teknologi yang terjadi saat ini, semua tugas-tugas divisi SDM dapat menjadi lebih ringan dengan hadirnya Sistem Informasi Sumber Daya Manusia atau SISDM.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <?php include($_SERVER['DOCUMENT_ROOT'] . '/ind/absensi-app-online-section.php'); ?>
+
+
+    <section class="margintop-80 color--blue">
+        <div class="container">
+
+            <div class="text-center title--bg title--bg-border title--bg-border-left" data-title-bg="DEFINISI SISDM">
+                <h2 class="mb-0 font-weight-bold fs-34">Definisi SISDM</h2>
+            </div>
+
+            <div class="fs-14 row justify-content-center">
+                <div class="col-md-10">
+                    <p class="text-base">SISDM atau Sistem Informasi Sumber Daya Manusia adalah sistem atau program yang digunakan untuk mengorganisir dan mengelola data SDM di perusahaan atau instansi. Perusahaan dapat menggunakannya untuk menyimpan, memproses, dan mengelola data karyawan dengan mudah. Perlu Anda tahu, SISDM ini juga dikenal dengan istilah lain yaitu HRIS atau Human Resource Information Systems.</p>
+                    <p class="text-base mb-0">SISDM yang komprehensif menyediakan semua informasi / data karyawan pada satu tempat serta memiliki kemampuan analisis dan pelaporan yang terintegrasi. Oleh karena itulah, SISDM juga sering dimanfaatkan untuk mendukung proses pengambilan keputusan atau decision support system seperti pemberian bonus, promosi, atau yang lain karena dapat menyediakan berbagai informasi yang diperlukan terkait karyawan.</p>
+                </div>
+            </div>
+
+        </div>
+    </section>
+
+    <section class="margintop-80  color--blue">
+        <div class="container">
+
+            <div class="text-center title--bg title--bg-border title--bg-border-right" data-title-bg="FITUR UTAMA">
+                <h2 class="mb-0 font-weight-bold fs-34">Fitur-fitur Utama yang Ada di dalam SISDM</h2>
+            </div>
+
+            <div class="fs-14 row justify-content-center">
+                <div class="col-md-10">
+
+                    <h3 class="margintop-80 mb-2 font-weight-bold">1. Centralized storage</h3>
+                    <p>Aplikasi SISDM atau HRIS dapat mengumpulkan, menyimpan, dan menampilkan data terbaru terkait karyawan ataupun kebijakan sebuah perusahaan. Semua data tersebut akan tersimpan pada satu tempat pusat data sehingga dapat memudahkan akses pengguna akhir.</p>
+              
+                    <h3 class="margintop-80 mb-2 font-weight-bold">2. Recruitment management</h3>
+                    <p>Fitur ini dapat memudahkan divisi SDM dalam proses rekrutmen. SISDM dapat membantu Anda untuk menyusun tahapan rekrutmen mulai dari verifikasi informasi, interview, sampai proses onboarding atau penerimaan karyawan.</p>
+
+                    <h3 class="margintop-80 mb-2 font-weight-bold">3. Employee onboarding</h3>
+                    <p>SISDM juga menyediakan fitur yang user-friendly untuk employee onboarding. Perusahaan dapat menjelaskan kebijakan perusahaan dan mengumpulkan data karyawan baru dengan mudah melalui sistem.</p>
+     
+                    <h3 class="margintop-80 mb-2 font-weight-bold">4. Talent management</h3>
+                    <p>Karyawan termasuk sumber daya yang berharga di dalam perusahaan. Namun untuk mengelola, mengembangkan, adn mempertahankan karyawan adalah tugas yang kompleks. SISDM dapat membantu Anda untuk melakukannya. Sistem SISDM biasanya akan menyediakan fitur talent management yang bisa Anda pergunakan untuk berbagai hal seperti melihat jumlah jabatan yang kosong, mengidentifikasi kesenjangan keterampilan, mengelola aktivitas pengembangan keterampilan, dan lain-lain.</p>
+
+                    <h3 class="margintop-80 mb-2 font-weight-bold">5. Employee Self-Service</h3>
+                    <p>Employee Self-Service atau ESS merupakan fitur yang memungkinkan karyawan untuk melakukan fungsi HR secara mandiri. Fitur ESS pada Sistem Informasi Sumber Daya Manusia pada umumnya diberikan dalam beberapa bentuk layanan seperti untuk personalisasi data, pengajuan cuti atau lembur, reimbursement, serta menerima slip gaji.</p>
+
+                    <h3 class="margintop-80 mb-2 font-weight-bold">6. Time and Absence Management</h3>
+                    <p>SISDM dapat mengelola dan melacak absensi karyawan karena dapat dipergunakan sebagai aplikasi absensi online. Dengan fitur tersebut, sistem dapat memberikan berbagai informasi seperti jam kedatangan / jam keterlambatan, total jam kerja, serta informasi terkait ketidakhadiran karyawan baik itu karena alasan izin, cuti, ataupun sakit. Selain itu, melalui SISDM karyawan juga bisa mengajukan cuti tanpa perlu melalui proses yang panjang.</p>
+
+                    <h3 class="margintop-80 mb-2 font-weight-bold">7. Workforce analytics</h3>
+                    <p>SISDM akan memberikan laporan yang berisi berbagai insight mengenai tenaga kerja di perusahaan Anda. Beberapa contoh laporan yang tersedia seperti rangkuman absensi karyawan, total jam kerja, kegiatan karyawan, atau yang lain. Fitur ini tentu akan sangat membantu karena dapat dipergunakan untuk melihat bagaimana kinerja karyawan. Selain itu, fitur ini juga dapat membantu divisi SDM dalam pembuatan laporan bulanan.</p>
+                    
+                    <p class="margintop-80 mb-5">Dari berbagai fitur yang sudah kami ajarkan di atas, dapat disimpulkan bahwa SISDM atau HRIS ini merupakan sistem cerdas yang dapat merampingkan proses manajemen SDM. Sistem ini memiliki banyak fitur yang dapat membantu berbagai tugas, sehingga divisi SDM dapat fokus pada tugas-tugas yang lain.</p> 
+
+                </div>
+            </div>
+
+            <div class="text-center title--bg title--bg-border title--bg-border-left" data-title-bg="SEDANG">
+                <h2 class="mb-0 font-weight-bold fs-34">Sedang mempertimbangkan penggunaan SISDM/HRIS di perusahaan Anda?</h2>
+            </div>
+
+            <div class="fs-14 row justify-content-center">
+                <div class="col-md-10">
+                    <p class="text-base text-center mb-0">Jadwalkan Konsultasi Dengan Tim Kami Sekarang!</p>
+                </div>
+            </div>
+
+            <div class="col-sm-6 col-md-6 col-lg-6 mx-auto mb-5">
+                <a href="/ind/contact-us" class="form-control btn btn--rounded bg--yellow mb-2 mt-4">Hubungi Kami</a>
+            </div> 
+
+        </div>
+    </section>
+
+</div>
+
+<?php include($_SERVER['DOCUMENT_ROOT'] . '/ind/component/demo-banner.php'); ?>
+<?php include($_SERVER['DOCUMENT_ROOT'] . '/ind/component/footer.php'); ?>
+
+<script>
+    $(function() {
+        $(".case-study__slide").slick({
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            arrows: true,
+            responsive: [{
+                    breakpoint: 991,
+                    settings: {
+                        slidesToShow: 2,
+                    }
+                },
+                {
+                    breakpoint: 767,
+                    settings: {
+                        slidesToShow: 1,
+                    }
+                }
+                // You can unslick at a given breakpoint now by adding:
+                // settings: "unslick"
+                // instead of a settings object
+            ]
+
+        })
+    });
+</script>
+
+<script type="application/ld+json">
+{
+  "@context": "https://schema.org",
+  "@type": "Article",
+  "mainEntityOfPage": {
+    "@type": "WebPage",
+    "@id": "https://dokodemo-kerja.com/ind/jenis-software-hris"
+  },
+  "headline": "Jenis-jenis Software HRIS yang ada di Indonesia",
+  "description": "Terdapat beberapa jenis software HRIS di Indonesia yang mampu merampingkan manajemen SDM sehingga proses pengelolaan karyawan dapat dilakukan dengan cepat dan mudah. Software ini memungkinkan tim HRD ( Human Resource Development) untuk menyimpan dan mengatur data berskala besar yang berkaitan dengan informasi karyawan. Selain itu, software HRIS di Indonesia juga dapat digunakan untuk meningkatkan produktivitas dan kedisiplinan karyawan terhadap peraturan-peraturan di kantor. Berikut diantaranya; 1. Operational HRIS, 2. Tactical HRIS, 3. Strategic HRIS, dan 4. Comprehensive HRIS.",
+  "image": "https://www.dokodemo-kerja.com/img/swiper-img/jenis-jenis-hris.png",  
+  "author": {
+    "@type": "Organization",
+    "name": "Dokodemo-Kerja"
+  },  
+  "publisher": {
+    "@type": "Organization",
+    "name": "Logique",
+    "logo": {
+      "@type": "ImageObject",
+      "url": "https://www.dokodemo-kerja.com/img/lgq-white.png"
+    }
+  },
+  "datePublished": "2021-05-16",
+  "dateModified": "2021-05-27"
+}
+</script>
+
+<?php include($_SERVER['DOCUMENT_ROOT'] . '/component/footer-end.php') ?>
