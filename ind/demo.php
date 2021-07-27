58762ab870f3d5de65bad5059a5613f9053e409a
abdul_rachman <abdul.rachman@logique.co.id>
2021-07-27 14:44:13 +0700

    refactor: optimize about page, demo page, adding attribute lazy load

--- ind/demo.php
+++ ind/demo.php
@@ -60,7 +60,7 @@ require '../action/c-trial.php';
 
 	<section class="intro-section text-center px-4">
 		<h2 class="banner-text-white">Coba Demo Sekarang. Gratis !</h2>
-		<img src="/img/home/arrow-down.svg" class="pt-4 pb-3" width="20" alt=""/>
+		<img src="/img/home/arrow-down.svg" class="pt-4 pb-3" width="20" alt="" loading="lazy" />
 		<a href="https://demo.dokodemo-kerja.com" class="btn btn-trial-contact bold uppercase" target="_blank" rel="noopener noreferrer" id="btn-demo-trial-id">Admin Demo</a>
 		<a href="https://drive.google.com/file/d/1itRszKytvu-x6RxoN0Ba1i_dNvtFlEHF/view?usp=sharing" target="_blank" rel="noopener noreferrer">
 			<p class="trial-header-content">Untuk mencoba versi staff, silahkan ikut instruksi dibawah</p>
@@ -71,12 +71,26 @@ require '../action/c-trial.php';
 		<div class="container">
 			<div class="row mt-5 mb-5">
 				<div class="offset-md-1 col-md-10 tutor-container mt-4 mb-3">
-					<!-- <iframe title="trial youtube video" src="https://www.youtube.com/embed/Qyz0FtNbVbI" class="trial-iframe" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> -->
 					<div class="embed-responsive embed-responsive-16by9 feature-video">
-                        <iframe class="embed-responsive-item" width="811" height="417" title="Dokodemo Youtube Video"
+                        <!-- <iframe 
+							loading="lazy"
+							class="embed-responsive-item" width="811" height="417" title="Dokodemo Youtube Video"
                             src="https://www.youtube.com/embed/ZRU2cc7lWbw" frameborder="0"
                             allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                            allowfullscreen></iframe>
+                            allowfullscreen>
+						</iframe> -->
+
+						<iframe
+							loading="lazy"
+							class="embed-responsive-item" width="811" height="417" title="Dokodemo Youtube Video"
+							src="https://www.youtube.com/embed/ZRU2cc7lWbw"
+							srcdoc="<style>*{padding:0;margin:0;overflow:hidden}html,body{height:100%}img,span{position:absolute;width:100%;top:0;bottom:0;margin:auto}span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;text-shadow:0 0 0.5em black}</style><a href=https://www.youtube.com/embed/ZRU2cc7lWbw?autoplay=1><img src=https://img.youtube.com/vi/ZRU2cc7lWbw/hqdefault.jpg alt='Dokodemo Youtube Video'><span>▶</span></a>"
+							frameborder="0"
+							allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+							allowfullscreen							
+							>
+						</iframe>
+
                     </div>	
 				</div>
 				<div class="offset-md-1 col-md-10 download-container text-center">
@@ -130,7 +144,7 @@ require '../action/c-trial.php';
 					<div class="installation-manual">
 						Download user manual di sini:
 						<a class="installation-manual__item" target="_blank" href="/pdf/user_manual_dokodemo_ver_2-2-2-id.pdf">
-							<img src="/img/trial/pdf.svg" alt="">
+							<img src="/img/trial/pdf.svg" alt="" loading="lazy" />
 							Desktop
 						</a>
 					</div>
@@ -163,7 +177,7 @@ require '../action/c-trial.php';
 												<p class="trial-tab-content">Login dengan email dan password yang tersedia.</p>
 												<picture>
 													<source srcset="/img/trial/admin-1.webp" type="image/webp">
-													<img src="/img/trial/admin-1.jpg" alt="Dokodemo-Kerja Website Login" class="img-fluid img-border mb-3">
+													<img src="/img/trial/admin-1.jpg" alt="Dokodemo-Kerja Website Login" class="img-fluid img-border mb-3" loading="lazy" />
 												</picture>
 											</div>
 										</div>
@@ -175,21 +189,21 @@ require '../action/c-trial.php';
 										<p class="trial-tab-content">Dapat diakses melalui <strong>Profile</strong> > <strong>Settings</strong> > <strong>Task</strong>.</p>
 										<picture>
 											<source srcset="/img/trial/admin-3-1.webp" type="image/webp">
-											<img src="/img/trial/admin-3-1.jpg" alt="Dokodemo-Kerja Drop Down Account Access Option" class="mb-3 img-fluid w-100 img-border">
+											<img src="/img/trial/admin-3-1.jpg" alt="Dokodemo-Kerja Drop Down Account Access Option" class="mb-3 img-fluid w-100 img-border" loading="lazy" />
 										</picture>
 										<div class="row">
 											<div class="col-md-6">
 												<p class="trial-tab-content">Tekan tombol "<strong>Add New</strong>" untuk menambahkan Task baru. </p>
 												<picture>
 													<source srcset="/img/trial/admin-3-2.webp" type="image/webp">
-													<img src="/img/trial/admin-3-2.jpg" alt="Dokodemo-Kerja 'Add New' Button" class="mb-3 img-fluid w-100 img-border">
+													<img src="/img/trial/admin-3-2.jpg" alt="Dokodemo-Kerja 'Add New' Button" class="mb-3 img-fluid w-100 img-border" loading="lazy" />
 												</picture>
 											</div>
 											<div class="col-md-6">
 												<p class="trial-tab-content">Isi informasi task baru, tekan Submit, dan Task Baru telah ditambahkan. </p>
 												<picture>
 													<source srcset="/img/trial/admin-3-3.webp" type="image/webp">
-													<img src="/img/trial/admin-3-3.jpg" alt="Dokodemo-Kerja Install Guide" class="mb-5 img-fluid w-100 img-border">
+													<img src="/img/trial/admin-3-3.jpg" alt="Dokodemo-Kerja Install Guide" class="mb-5 img-fluid w-100 img-border" loading="lazy" />
 												</picture>
 											</div>
 										</div>
@@ -201,21 +215,21 @@ require '../action/c-trial.php';
 										<p class="trial-tab-content">Dapat diakses melalui <strong>Profile</strong> > <strong>Settings</strong> > <strong>Team</strong>.</p>
 										<picture>
 											<source srcset="/img/trial/admin-4-1.webp" type="image/webp">
-											<img src="/img/trial/admin-4-1.jpg" alt="Dokodemo-Kerja Drop Down Account Access Option" class="mb-3 img-fluid w-100 img-border">
+											<img src="/img/trial/admin-4-1.jpg" alt="Dokodemo-Kerja Drop Down Account Access Option" class="mb-3 img-fluid w-100 img-border" loading="lazy" />
 										</picture>
 										<div class="row">
 											<div class="col-md-6">
 												<p class="trial-tab-content">Tekan tombol "<strong>Add New</strong>" untuk menambahkan Team baru.</p>
 												<picture>
 													<source srcset="/img/trial/admin-4-2.webp" type="image/webp">
-													<img src="/img/trial/admin-4-2.jpg" alt="Dokodemo-Kerja 'Add New' Button" class="mb-5 img-fluid w-100 img-border">
+													<img src="/img/trial/admin-4-2.jpg" alt="Dokodemo-Kerja 'Add New' Button" class="mb-5 img-fluid w-100 img-border" />
 												</picture>
 											</div>
 											<div class="col-md-6">
 												<p class="trial-tab-content">Isi nama team dan leader, tekan Submit, dan Team Baru telah ditambahkan.</p>
 												<picture>
 													<source srcset="/img/trial/admin-4-3.webp" type="image/webp">
-													<img src="/img/trial/admin-4-3.jpg" alt="Dokodemo-Kerja Add Team Management" class="mb-5 img-fluid w-100 img-border">
+													<img src="/img/trial/admin-4-3.jpg" alt="Dokodemo-Kerja Add Team Management" class="mb-5 img-fluid w-100 img-border" loading="lazy" />
 												</picture>
 											</div>
 										</div>
@@ -227,28 +241,28 @@ require '../action/c-trial.php';
 										<p class="trial-tab-content">Dapat diakses melalui <strong>Profile</strong> > <strong>Settings</strong> > <strong>Staff</strong>.</p>
 										<picture>
 											<source srcset="/img/trial/admin-2-1.webp" type="image/webp">
-											<img src="/img/trial/admin-2-1.jpg" alt="Dokodemo-Kerja Drop Down Account Access Option" class="mb-3 img-fluid w-100 img-border">
+											<img src="/img/trial/admin-2-1.jpg" alt="Dokodemo-Kerja Drop Down Account Access Option" class="mb-3 img-fluid w-100 img-border" loading="lazy" />
 										</picture>
 										<div class="row">
 											<div class="col-md-6">
 												<p class="trial-tab-content">Tekan tombol "<strong>Add New</strong>" untuk menambahkan Staff baru.</p>
 												<picture>
 													<source srcset="/img/trial/admin-2-2.webp" type="image/webp">
-													<img src="/img/trial/admin-2-2.jpg" alt="Dokodemo-Kerja 'Add New' Button" class="mb-3 img-fluid w-100 img-border">
+													<img src="/img/trial/admin-2-2.jpg" alt="Dokodemo-Kerja 'Add New' Button" class="mb-3 img-fluid w-100 img-border" loading="lazy" />
 												</picture>
 											</div>
 											<div class="col-md-6">
 												<p class="trial-tab-content">Masukkan informasi Staff baru dan klik "<strong>Submit</strong>".</p>
 												<picture>
 													<source srcset="/img/trial/admin-2-3.webp" type="image/webp">
-													<img src="/img/trial/admin-2-3.jpg" alt="Dokodemo-Kerja's Add Staff option" class="mb-3 img-fluid w-100 img-border">
+													<img src="/img/trial/admin-2-3.jpg" alt="Dokodemo-Kerja's Add Staff option" class="mb-3 img-fluid w-100 img-border" loading="lazy" />
 												</picture>
 											</div>
 										</div>
 										<p class="trial-tab-content">Informasi password untuk masuk ke Dokodemo-Kerja akan dikirimkan ke alamat E-mail terdaftar. Dengan informasi tersebut maka Anda akan dapat menggunakan Dokodemo-Kerja.</p>
 										<picture>
 											<source srcset="/img/trial/admin-2-4.webp" type="image/webp">
-											<img src="/img/trial/admin-2-4.jpg" alt="Dokodemo-Kerja Staff Management" class="mb-5 img-fluid w-100 img-border">
+											<img src="/img/trial/admin-2-4.jpg" alt="Dokodemo-Kerja Staff Management" class="mb-5 img-fluid w-100 img-border" loading="lazy" />
 										</picture>
 									</div>
 								</div>
@@ -264,11 +278,11 @@ require '../action/c-trial.php';
 									<div class="row">
 										<div class="col-md-6">
 											<p class="trial-tab-content">Extract folder demo dan cari program Dokodemo-Kerja.</p>
-											<img src="/img/trial/staff-1.png" class="mb-3 img-fluid w-100 img-border" alt="Dokodemo-Kerja Zip File"/>
+											<img src="/img/trial/staff-1.png" class="mb-3 img-fluid w-100 img-border" alt="Dokodemo-Kerja Zip File" loading="lazy" />
 										</div>
 										<div class="col-md-6">
 											<p class="trial-tab-content">Jalankan aplikasinya. <br class="d-none d-md-block"><br class="d-none d-md-block"></p>
-											<img src="/img/trial/staff-2.png" class="mb-3 img-fluid w-100 img-border" alt="Downloaded Dokodemo-Kerja App Icon"/>
+											<img src="/img/trial/staff-2.png" class="mb-3 img-fluid w-100 img-border" alt="Downloaded Dokodemo-Kerja App Icon" loading="lazy" />
 										</div>
 									</div>
 								</div>
@@ -282,13 +296,13 @@ require '../action/c-trial.php';
 													Masuk dengan menggunakan informasi yang dikirimkan melalui Email.
 												</p>
 											</div>
-											<img src="/img/trial/staff-3.png" class="mb-3 img-fluid w-100 img-border" alt="Dokodemo-Kerja Version 2.0.0"/>
+											<img src="/img/trial/staff-3.png" class="mb-3 img-fluid w-100 img-border" alt="Dokodemo-Kerja Version 2.0.0" loading="lazy" />
 										</div>
 										<div class="col-md-6">
 											<p class="trial-tab-content">Silahkan mulai bekerja dengan memilih "<strong>Task</strong>" dan tekan tombol "<strong>Play</strong>"</p>
-											<img src="/img/trial/staff-4.png" class="mb-3 img-fluid w-100 img-border" alt="Dokodemo-Kerja Application"/>
+											<img src="/img/trial/staff-4.png" class="mb-3 img-fluid w-100 img-border" alt="Dokodemo-Kerja Application" loading="lazy" />
 											<p class="trial-tab-content">Tekan tombol "<strong>Stop</strong>" (warna merah) untuk berhenti</p>
-											<img src="/img/trial/staff-5.png" class="mb-5 img-fluid w-100 img-border" alt="Dokodemo-Kerja Application Appearance"/>
+											<img src="/img/trial/staff-5.png" class="mb-5 img-fluid w-100 img-border" alt="Dokodemo-Kerja Application Appearance" loading="lazy" />
 										</div>
 									</div>
 								</div>
@@ -324,7 +338,7 @@ require '../action/c-trial.php';
 											</div>
 										</div>
 										<div class="col-md-6">
-											<img src="/img/trial/staff-6.png" class="mb-3 img-fluid w-100 img-border" alt="Dokodemo-Kerja Application"/>
+											<img src="/img/trial/staff-6.png" class="mb-3 img-fluid w-100 img-border" alt="Dokodemo-Kerja Application" loading="lazy" />
 										</div>
 									</div>
 								</div>
@@ -332,7 +346,7 @@ require '../action/c-trial.php';
 								<h2 class="content-section-title">4. Tampilan Layar</h2>
 								<div class="pl-4">
 									<p class="trial-tab-content">Anda bisa melihat berapa lama anda telah bekerja pada suatu task. Ikon yang ada di dalam kolom berwarna adalah untuk mengubah waktu secara manual. Selain itu, terdapat waktu riwayat pengubahan waktu serta hasil tangkapan layar dibagian bawah.</p>
-									<img src="/img/trial/staff-7.png" class="mb-5 img-fluid w-100 img-border" alt="Dokodemo-Kerja Time Tracking"/>
+									<img src="/img/trial/staff-7.png" class="mb-5 img-fluid w-100 img-border" alt="Dokodemo-Kerja Time Tracking" />
 								</div>
 							</div>
 						</div>
@@ -346,13 +360,13 @@ require '../action/c-trial.php';
 										<div class="col-md-6">
 											<p class="trial-tab-content">Cari aplikasi "Dokodemo Mobile" di Play Store / App Store, dan tekan "Install"</p>
 											<div class="text-center">
-												<img src="/img/trial/mobile-1.png" class="mb-3 img-fluid mobile-screenshot img-border" alt="Download Dokodemo-Kerja from the Google Play Store"/>
+												<img src="/img/trial/mobile-1.png" class="mb-3 img-fluid mobile-screenshot img-border" alt="Download Dokodemo-Kerja from the Google Play Store" />
 											</div>
 										</div>
 										<div class="col-md-6">
 											<!-- <p class="trial-tab-content">Atau website Dokodemo-Kerja. <br><br></p> -->
 											<p class="trial-tab-content">Atau download aplikasi mobile dari website Dokodemo perusahaan.</p>
-											<img src="/img/trial/mobile-2.png" class="mb-3 img-fluid mobile-screenshot img-border" alt="Download Dokodemo-Kerja Mobile for iOS or Android"/>
+											<img src="/img/trial/mobile-2.png" class="mb-3 img-fluid mobile-screenshot img-border" alt="Download Dokodemo-Kerja Mobile for iOS or Android" />
 										</div>
 									</div>
 								</div>
@@ -365,7 +379,7 @@ require '../action/c-trial.php';
 												<p class="mb-0">Login dengan Company Code (DKDM), E-mail dan Password yang sudah didaftarkan di website demo Admin, kemudian tekan tombol Login.</p>
 											</div>
 											<div class="text-center">
-												<img src="/img/trial/mobile-3.png" class="mb-3 img-fluid img-border mobile-screenshot" alt="Dokodemo-Kerja Login Page"/>
+												<img src="/img/trial/mobile-3.png" class="mb-3 img-fluid img-border mobile-screenshot" alt="Dokodemo-Kerja Login Page" loading="lazy" />
 											</div>
 										</div>
 										<div class="col-md-6">
@@ -375,7 +389,7 @@ require '../action/c-trial.php';
 											<br class="d-none d-md-block"><br class="d-none d-md-block">
 											<br class="d-none d-md-block d-lg-none"><br class="d-none d-md-block d-lg-none"></p>
 											<div class="text-center">
-												<img src="/img/trial/mobile-4.png" class="mb-3 img-fluid img-border mobile-screenshot" alt="Dokodemo-Kerja Task Selection"/>
+												<img src="/img/trial/mobile-4.png" class="mb-3 img-fluid img-border mobile-screenshot" alt="Dokodemo-Kerja Task Selection" loading="lazy" />
 											</div>
 										</div>
 									</div>
@@ -390,7 +404,7 @@ require '../action/c-trial.php';
 													Beberapa fungsi yang tersedia pada Dokodemo-Kerja Mobile App 
 												</p>
 												<div class="text-center">
-													<img src="/img/trial/mobile-5.png" class="mb-3 img-fluid img-border mobile-screenshot" alt="Dokodemo-Kerja Mobile Staff Login"/>
+													<img src="/img/trial/mobile-5.png" class="mb-3 img-fluid img-border mobile-screenshot" alt="Dokodemo-Kerja Mobile Staff Login" loading="lazy" />
 												</div>
 											</div>
 										</div>
@@ -398,7 +412,7 @@ require '../action/c-trial.php';
 											<div class="trial-tab-content">
 												<p>Tombol “Start” dan “Stop” <br class="d-none d-md-block"><br class="d-none d-md-block"><br class="d-none d-md-block d-lg-none"></p>
 												<div class="text-center">
-													<img src="/img/trial/mobile-6.png" class="mb-3 img-fluid img-border mobile-screenshot" alt="Dokodemo-Kerja Play Button"/>
+													<img src="/img/trial/mobile-6.png" class="mb-3 img-fluid img-border mobile-screenshot" alt="Dokodemo-Kerja Play Button" loading="lazy" />
 												</div>
 											</div>
 										</div>
@@ -412,7 +426,7 @@ require '../action/c-trial.php';
 											<div class="trial-tab-content">
 												<p>GPS Tracker untuk melacak user saat melakukan kegiatan di luar kantor.</p>
 												<div class="text-center">
-													<img src="/img/trial/mobile-7.png" class="mb-3 img-fluid img-border mobile-screenshot" alt="Dokodemo-Kerja Mobile Location Tracking"/>
+													<img src="/img/trial/mobile-7.png" class="mb-3 img-fluid img-border mobile-screenshot" alt="Dokodemo-Kerja Mobile Location Tracking" loading="lazy" />
 												</div>
 											</div>
 										</div>
@@ -420,7 +434,7 @@ require '../action/c-trial.php';
 											<div class="trial-tab-content">
 												<p>Screenshot layar desktop saat sedang bekerja. (Desktop App)<br class="d-none d-md-block d-lg-none"><br class="d-none d-md-block d-lg-none"></p>
 												<div class="text-center">
-													<img src="/img/trial/mobile-8.png" class="mb-3 img-fluid img-border mobile-screenshot" alt="Dokodemo-Kerja Mobile Screen Recording Record"/>
+													<img src="/img/trial/mobile-8.png" class="mb-3 img-fluid img-border mobile-screenshot" alt="Dokodemo-Kerja Mobile Screen Recording Record" loading="lazy" />
 												</div>
 											</div>
 										</div>
@@ -434,7 +448,7 @@ require '../action/c-trial.php';
 												<h2 class="tab-content-title content-section-title">6. History Record</h2>
 												<p>User dapat melihat jam kerjanya secara Daily, Weekly dan Monthly.</p>
 												<div class="text-center">
-													<img src="/img/trial/mobile-9.png" class="mb-3 img-fluid img-border mobile-screenshot" alt="Dokodemo-Kerja Mobile Screen Record"/>
+													<img src="/img/trial/mobile-9.png" class="mb-3 img-fluid img-border mobile-screenshot" alt="Dokodemo-Kerja Mobile Screen Record" loading="lazy" />
 												</div>
 											</div>
 										</div>
@@ -443,7 +457,7 @@ require '../action/c-trial.php';
 												<h2 class="tab-content-title content-section-title">6. Account</h2>
 												<p>Menu ini menampilkan user profile, dan dapat digunakan untuk logout.</p>
 												<div class="text-center">
-													<img src="/img/trial/mobile-10.png" class="mb-3 img-fluid img-border mobile-screenshot" alt="Dokodemo-Kerja Mobile Login"/>
+													<img src="/img/trial/mobile-10.png" class="mb-3 img-fluid img-border mobile-screenshot" alt="Dokodemo-Kerja Mobile Login" loading="lazy" />
 												</div>
 											</div>
 										</div>
