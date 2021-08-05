5e18c2a2dad59b6b50a91aace059be2dda3ad52c
calvinocta <calvin@logique.co.id>
2021-08-05 09:41:09 +0700

    add fitur aplikasi hrd page indo

---
+++ ind/fitur-aplikasi-HRD.php
@@ -0,0 +1,168 @@
+<?php
+$page = array(
+    "en" => "features-manajemen-SISDM",
+    "jp" => "types-of-hris",
+);
+$page_title = 'Fitur-fitur Unggulan pada Aplikasi HRD | Dokodemo-Kerja';
+
+$meta_description = 'Aplikasi HRD Dokodemo-Kerja menyediakan beragam fitur unggulan seperti e-absensi, gps tracking, pengajuan cuti, dan lain-lain. Dapatkan info selengkapnya di sini!';
+$meta_keyword = 'dokodemo, dokodemo-kerja, Dokodemo kasus, Digital Logique Indonesia, sistem kerja jarak jauh, system kerja remote, perusahan pengembangan system, monitor karyawan, alat kerja remote, alat untuk manajer, perusahaan akuntansi';
+
+$og_title = 'Fitur-fitur Unggulan pada Aplikasi HRD | Dokodemo-Kerja';
+$og_image = 'https://www.dokodemo-kerja.com/ind/fitur-aplikasi-HRD';
+$og_desription = '';
+$canonical = 'https://www.dokodemo-kerja.com/ind/fitur-aplikasi-HRD';
+
+$add_css = '
+    <link rel="stylesheet" href="/css/slick.css" />
+    <link rel="stylesheet" href="/css/slick-theme.css" />
+    <link rel="stylesheet" href="/css/style-home.css" />
+    <link rel="stylesheet" href="/css/fitur-aplikasi-hrd/index.css" />
+    <link rel="stylesheet" href="/css/style-case-study.css" />';
+
+include($_SERVER['DOCUMENT_ROOT'] . '/ind/component/header.php');
+?>
+
+<div class="case-study home">
+    <section class="mb--30px fs-14">
+        <div class="container">
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb breadcrumb--main">
+                    <li class="breadcrumb-item"><a href="/ind/">Beranda</a></li>
+                    <li class="breadcrumb-item"><a href="/ind/info-hris">Info HRIS</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">Fitur Aplikasi HRD</li>
+                </ol>
+            </nav>
+        </div>
+    </section>
+
+    <section class="margintop-80">
+        <div class="color--blue container">
+            <div class="text-center title--bg title--bg-border title--bg-border-right" data-title-bg="FITUR UNGGULAN">
+                <h1 class="mb-0 font-weight-bold fs-34">5 Fitur Unggulan Aplikasi HRD Dokodemo-Kerja</h1>
+            </div>
+
+            <div class="fs-14 row justify-content-center">
+                <div class="col-md-10">
+                    <p class="text-base">Sudah semakin banyak perusahaan yang memanfaatkan aplikasi HRD untuk memperingkas aktivitas Human Resources Management (HRM). Di era digital, perusahaan memang sudah seharusnya dapat berpikir lebih modern. Jadi, penggunaan teknologi tidak hanya digunakan untuk menjalankan proses bisnis namun juga untuk mengelola karyawan di dalamnya. Melalui cara ini, perusahaan dapat menjalankan bisnis dengan efektif dan efisien secara keseluruhan. </p>
+                    <p class="text-base">Aplikasi HRD sendiri pada dasarnya merupakan salah satu jenis aplikasi bisnis yang memungkinkan perusahaan untuk dapat menyimpan informasi karyawan, mengelola fungsi umum tim HRD, serta membantu menjalankan aktivitas HRD. Saat ini, terdapat 3 jenis aplikasi HRD yang paling umum dikenal yaitu HRIS, HCM, dan HRMS. Untuk mengetahui definisi dari masing-masing teknologi tersebut, Anda dapat melihat informasinya pada artikel berikut:</p>
+                    <a href="/ind/teknologi-hris-hcm-hrms" class="link-blue d-block text-center mb-3 fs-20" target="_blank">"Perbedaan Mendasar antara Teknologi HRIS, HCM, dan HRMS"</a>
+                    <p class="text-base mb-0">Dokodemo-Kerja merupakan salah satu aplikasi HRD yang ada di Indonesia. Aplikasi ini menggunakan teknologi HRIS yang memungkinkan suatu perusahaan untuk dapat mengelola, menyimpan, dan memproses data karyawan secara digital. Jadi, apa saja fitur unggulan aplikasi HRD ini? Simak penjelasannya berikut.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <?php include($_SERVER['DOCUMENT_ROOT'] . '/ind/absensi-app-online-section.php'); ?>
+
+    <section class="margintop-80  color--blue">
+        <div class="container">
+
+            <div class="text-center title--bg title--bg-border title--bg-border-right" data-title-bg="FITUR UNGGULAN">
+                <h2 class="mb-0 font-weight-bold fs-34">Fitur Unggulan Aplikasi HRD Dokodemo-Kerja</h2>
+            </div>
+
+            <div class="fs-14 row justify-content-center">
+                <div class="col-md-10">
+
+                    <h3 class="margintop-80 mb-2 font-weight-bold">1. Fitur absensi online</h3>
+                    <p>Dokodemo-kerja memiliki fitur aplikasi absensi online (e-absensi). Jadi, dengan aplikasi ini karyawan dapat melakukan absensi secara digital kapanpun dan dimanapun secara real time.</p>
+                    <p>Untuk menggunakan fitur ini, karyawan cukup mengakses Dokodemo-Kerja yang sudah terinstal di perangkat mereka (desktop atau smartphone) dan menekan tombol "Play". Aplikasi kemudian akan mencatat waktu tersebut sebagai jam kedatangan / jam karyawan mulai bekerja.</p>
+                    <p>Dengan adanya fitur ini, Dokodemo-Kerja dapat menjadi aplikasi HRD yang sesuai untuk mengelola karyawan remote atau karyawan yang bekerja secara WFH (Work From Home). Selain itu, fitur ini juga mempermudah tim HRD untuk mengetahui siapa saja yang terlambat datang bekerja.</p>
+              
+                    <h3 class="margintop-80 mb-2 font-weight-bold">2. Mengambil screenshot saat karyawan bekerja</h3>
+                    <p>Sulit untuk memantau aktivitas karyawan saat bekerja? Jangan khawatir, Dokodemo-Kerja menyediakan fitur yang mampu mengambil screenshot pada layar perangkat karyawan.</p>
+                    <p>Dengan fitur ini, HRD atau pihak perusahaan dapat memonitor apa saja aktivitas karyawannya selama jam kerja. Tim HRD dapat mengetahui, apakah karyawannya benar-benar mengerjakan task yang diberikan atau justru menghabiskan jam kerjanya untuk melakukan hal-hal lain diluar tugasnya seperti terlalu lama membuka social media, menonton Youtube, atau yang lain.</p>
+                    <p>Meskipun demikian, Dokodemo-Kerja tetap akan menjaga privasi penggunanya. Oleh karena itu, hasil screenshot yang diambil menggunakan resolusi rendah sehingga teks atau komunikasi bisnis yang sedang terjadi tidak akan diketahui oleh departemen yang lain. Selain itu, aplikasi HRD ini juga akan mengambil screenshot secara acak dalam rentan waktu maksimal 10 menit.</p>
+
+                    <h3 class="margintop-80 mb-2 font-weight-bold">3. Menghitung total jam kerja karyawan</h3>
+                    <p>Dokodemo-Kerja juga menyediakan fitur untuk memonitor total jam kerja karyawan. Seperti yang kita ketahui, berdasarkan peraturan ketenagakerjaan yang diatur oleh Undang-Undang, total jam kerja karyawan dalam satu minggu adalah 40 jam. Artinya, perusahaan diperbolehkan untuk menggunakan sistem 7 jam kerja sehari untuk 6 hari kerja atau 8 jam kerja sehari untuk 5 hari kerja.</p>
+                    <p>Agar sebuah perusahaan dapat memantau total jam kerja karyawan dengan baik dan benar, perusahaan dapat menggunakan aplikasi Dokodemo-Kerja. Saat karyawan menekan tombol "Play", sistem akan mulai menghitung jam kerja karyawan dan akan berhenti menghitung saat karyawan menekan tombol "Stop". Selanjutnya, dari aplikasi Dokodemo-kerja, baik karyawan atau tim HRD dapat melihat berapa total jam kerja yang telah dilaksanakan secara harian, mingguan, atau bulanan.</p>
+                    <p>Melalui fitur ini, tim HRD juga dapat mengetahui siapa saja yang belum memenuhi jam kerja mingguan (40 jam) dan mengetahui siapa saja karyawan yang melaksanakan lembur. Dengan cara ini, tim HRD tidak hanya dapat memantau total jam kerja saja, namun juga mampu meningkatkan kedisiplinan.</p>
+     
+                    <h3 class="margintop-80 mb-2 font-weight-bold">4. Pengajuan cuti atau izin yang mudah</h3>
+                    <p>Aplikasi HRD Dokodemo-Kerja menghadirkan fitur untuk pengajuan cuti yang mudah dan sederhana. Sistem dapat menunjukkan berapa kuota cuti yang tersedia dan karyawan dapat dengan mudah mengajukan cuti langsung dari sistem tanpa harus membuat dokumen hardcopy yang rumit</p>
+                    <p>Di dalam sistem, Dokodemo-kerja menyediakan beragam pilihan cuti atau day off yang bisa dipilih oleh karyawan. Beberapa diantaranya seperti sick leave, paid leave, unpaid leave, government leave, dan lain-lain.</p>
+
+                    <h3 class="margintop-80 mb-2 font-weight-bold">5. GPS tracking</h3>
+                    <p>Dokodemo-Kerja memiliki fitur pelacakan GPS. Jadi, selain dapat memonitor karyawan yang bekerja di kantor, Dokodemo-Kerja juga memungkinkan untuk memonitor karyawan yang bekerja secara mobile. Dengan menginstal aplikasi HRD ini di perangkat smartphone, maka aktivitas karyawan di luar kantor tetap dapat Anda pantau dengan baik.</p>
+
+                    <p class="margintop-80 mb-5">Jika perusahaan dapat memanfaatkan dengan baik berbagai fitur di atas, perusahaan dapat terbantu dalam meningkatkan produktivitas karyawannya. Selain itu aplikasi ini juga mmeungkinkan karyawan untuk bekerja secara fleksibel dan mengelola jam kerja mereka dengan leluasa. Dengan demikian, karyawan juga akan memiliki Work Life Balance yang baik tanpa mengganggu produktivitas mereka saat bekerja.</p> 
+
+                </div>
+            </div>
+
+            <div class="fs-14 row justify-content-center">
+                <div class="col-md-10">
+                  <h3 class="mb-0 font-weight-bold fs-26 text-center">Tertarik untuk mengetahui bagaimana perusahaan-perusahaan di Indonesia memanfaatkan fitur-fitur Dokodemo-Kerja?</h2>
+                </div>
+            </div>
+
+            <div class="col-sm-6 col-md-6 col-lg-6 mx-auto mb-5">
+                <a href="/ind/case-study/" class="form-control btn btn--rounded bg--yellow mb-2 mt-4">Lihat Pengalaman Bekerja dengan Dokodemo-Kerja Selengkapnya</a>
+            </div> 
+
+        </div>
+    </section>
+
+</div>
+
+<?php include($_SERVER['DOCUMENT_ROOT'] . '/ind/component/demo-banner.php'); ?>
+<?php include($_SERVER['DOCUMENT_ROOT'] . '/ind/component/footer.php'); ?>
+
+<script>
+    $(function() {
+        $(".case-study__slide").slick({
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            arrows: true,
+            responsive: [{
+                    breakpoint: 991,
+                    settings: {
+                        slidesToShow: 2,
+                    }
+                },
+                {
+                    breakpoint: 767,
+                    settings: {
+                        slidesToShow: 1,
+                    }
+                }
+                // You can unslick at a given breakpoint now by adding:
+                // settings: "unslick"
+                // instead of a settings object
+            ]
+
+        })
+    });
+</script>
+
+<script type="application/ld+json">
+{
+  "@context": "https://schema.org",
+  "@type": "NewsArticle",
+  "mainEntityOfPage": {
+    "@type": "WebPage",
+    "@id": "https://dokodemo-kerja.com/ind/fitur-manajemen-SISDM"
+  },
+  "headline": "Manajemen Sistem Informasi SDM | Dokodemo-Kerja",
+  "description": "Sistem Informasi Sumber Daya Manusia atau SISDM adalah salah satu jenis perangkat lunak yang dibutuhkan oleh divisi SDM. Perangkat lunak ini menyediakan berbagai fitur yang berguna untuk proses manajemen SDM seperti mengelola dan memantau kinerja karyawan secara ringkas dan sederhana.",
+  "image": "https://www.dokodemo-kerja.com/img/swiper-img/aplikasi-hris.jpg",  
+  "author": {
+    "@type": "Organization",
+    "name": "Dokodemo-Kerja"
+  },  
+  "publisher": {
+    "@type": "Organization",
+    "name": "Logique",
+    "logo": {
+      "@type": "ImageObject",
+      "url": "https://www.dokodemo-kerja.com/img/lgq-white.png"
+    }
+  },
+  "datePublished": "2021-06-30",
+  "dateModified": "2021-07-10"
+}
+</script>
+
+<?php include($_SERVER['DOCUMENT_ROOT'] . '/component/footer-end.php') ?>
