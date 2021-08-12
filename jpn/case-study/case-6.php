81afcdd5b5e7b9507c9cbd6b7dd971a95e69fb3c
abdul_rachman <abdul.rachman@logique.co.id>
2021-08-12 17:10:11 +0700

    feat: case study 6

---
+++ jpn/case-study/case-6.php
@@ -0,0 +1,105 @@
+<?php 
+    $page = 'case-study/keunggulan-fitur-screenshoot';
+    $page_title = 'Keunggulan Fitur Screenshot Aplikasi HR Dokodemo-Kerja';
+
+    $meta_description = 'Aplikasi HR Dokodemo-Kerja terus dikembangkan untuk meningkatkan kualitas fungsi dan manfaatnya. Keunggulan terbaru terdapat pada fungsi monitoring melalui fitur screenshot yang lebih akurat';
+    $meta_keyword = 'dokodemo, dokodemo-kerja, kasus dokodemo, dokodemo story, alat kantor akuntan, logique digital Indonesia, THR, pemantauan karyawan, manajemen SDM, manajemen HR, alat manajemen, departemen SDM, departemen HR';
+
+    $og_title = 'Keunggulan Fitur Screenshot Aplikasi HR Dokodemo-Kerja';
+    $og_image = 'https://dokodemo-kerja.com/img/dokodemo-og.jpg';
+    $og_desription = 'Aplikasi HR Dokodemo-Kerja terus dikembangkan untuk meningkatkan kualitas fungsi dan manfaatnya. Keunggulan terbaru terdapat pada fungsi monitoring melalui fitur screenshot yang lebih akurat';
+    $add_css = '<link rel="stylesheet" href="/css/style-case-study.css" />';
+    include($_SERVER['DOCUMENT_ROOT'].'/jpn/component/header.php');
+?>
+
+<div class="">
+    <section>
+        <div class="container">
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb breadcrumb--main">
+                    <li class="breadcrumb-item"><a href="/ind/">Beranda</a></li>
+                    <li class="breadcrumb-item"><a href="/ind/case-study/">Studi Kasus</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">Kasus 6</li>
+                </ol>
+            </nav>
+        </div>
+    </section>
+
+    <section class="mb-5">
+        <div class="container">
+            <h1 class="text-center mb-5">Dokodemo-Kerja ストーリー6：<br>Dokodemo-Kerja バージョン2.1.3の新機能紹介: 同じスクリーンショットを検知する機能</h1>
+            <div class="case-study__story">
+                <blockquote class="blockquote text-center mb-5">
+                    <!-- <p class="mb-0">“Agar dapat menggunakan Dokodemo-kerja secara efektif, maka karyawan diharuskan untuk menggunakan Dokodemo-Kerja secara akurat. Ketika pengguna lupa menekan tombol "Stop" pada saat istirahat, maka hal tersebut dapat menyebabkan proses akumulasi serta perencanaan jam kerja untuk masing-masing tugas menjadi tidak tepat. Oleh karena itu, kami menyarankan agar pihak administrator segera menghubungi pengguna untuk mengkonfirmasi apabila terdapat 3 screenshot yang sama pada sistem secara berurutan, dan fitur baru yang tersedia dari Dokodemo-kerja saat ini dapat membuat proses konfirmasi tersebut menjadi lebih mudah.”</p> -->
+                    <!-- <div class="blockquote-footer"><cite title="Source Title">Operation Director</cite></div> -->
+                </blockquote>
+               
+                <div class="row">
+                    <div class="offset-md-1 col-md-10">
+                        <section class="text-justify">
+                            <div class="mb-4">
+                                <h2>
+                                    <b>
+                                        Dokodemo-Kerjaを効果的に使うために、従業員は正確に
+                                    </b>
+                                </h2>
+                                <p>
+                                    Dokodemo-Kerjaを利用することが求められます。休憩を取る際にDokodemo-KerjaのStopボタンを押し忘れたりすると勤務時間やタスクごとの消化工数が誤って計算されてしまいます。そこで、3枚同じスクリーンショットが並んだ時に管理者からユーザーに確認の連絡をすることをお奨めしているのですが、新機能はこの確認を簡単にします。
+                                </p>
+                                <p>
+                                    いままで、多くのユーザーがいる会社でこの（社員のスクリーンショット画像を動的にチェックする）作業をするのはなかなか大変でした。
+                                </p>
+                                <p class="mb-5">
+                                    新機能では、これを自動で認識し通知します。
+                                </p>
+                            </div>
+                            <div class="mb-4">
+                                <picture>
+                                    <source media="(min-width: 1024px)" srcset="/img/case-study/6/image-1.jpg">
+                                    <source media="(max-width: 640px)" srcset="/img/case-study/6/image-1.jpg">
+                                    <img style="border-radius: 5px; box-shadow:1px 3px 3px gray;" width="100%" height="auto" src="/img/case-study/6/image-1.jpg" alt="banner">
+                                </picture> 
+                                <p class="mt-3">
+                                    同じ画像が3枚以上並ぶと赤字でマークされ、管理者にNotificationが送られます。
+                                </p>
+                            </div>
+                            
+                            <div class="mb-4">
+                                <picture>
+                                    <source media="(min-width: 1024px)" srcset="/img/case-study/6/image-2.jpg">
+                                    <source media="(max-width: 640px)" srcset="/img/case-study/6/image-2.jpg">
+                                    <img style="border-radius: 5px; box-shadow:1px 3px 3px gray;" width="100%" height="auto" src="/img/case-study/6/image-1.jpg" alt="banner">
+                                </picture> 
+                                <p class="mt-5">
+                                    確認の結果、画面が変わらなかっただけで働いていた場合（電話対応をしていた等）にはもちろん対応の必要ありませんが、停止し忘れていた等の場合には、勤務時間を編集してもらうことになります。
+                                </p>
+                                <p>
+                                    小さな新機能ですが、管理者の労力を大幅に減らす便利な機能だと思いますのでご紹介しました。
+                                </p>
+                                <p>
+                                    まだご利用にいただけてない方は、デモをご利用いただき確認いただければ幸いです。お気軽にお問い合わせください。
+                                </p>
+                            </div>                         
+                            <hr />
+                            <div class="mb-4 text-center">
+                                <p>
+                                    Tertarik untuk mengetahui fitur Dokodemo-Kerja yang lain?
+                                </p>                               
+                                <a href="https://dokodemo-kerja.com/ind/fitur">                            
+                                    <button type="button" class="btn btn-sm btn-primary text-center">                                    
+                                        Lihat Fitur Dokodemo-Kerja Selengkapnya di Sini!
+                                    </button>
+                                </a>
+                            </div>
+                        </section>
+                    </div>
+                </div>                    
+            </div>
+        </div>
+    </section>
+</div>
+
+<?php include($_SERVER['DOCUMENT_ROOT'].'/ind/case-study/other-case.php'); ?>
+<?php include($_SERVER['DOCUMENT_ROOT'].'/ind/component/demo-banner.php'); ?>
+<?php include($_SERVER['DOCUMENT_ROOT'].'/ind/component/footer.php'); ?>
+<?php include($_SERVER['DOCUMENT_ROOT'].'/component/footer-end.php') ?>
