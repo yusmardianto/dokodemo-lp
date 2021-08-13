7ed90c2f65740426c2d74d8fdd2da1c4468aadd0
abdul_rachman <abdul.rachman@logique.co.id>
2021-08-13 14:08:44 +0700

    feat: adding EN version case-study 6 + card carousel

---
+++ case-study/excellence-screenshoot-feature.php
@@ -0,0 +1,111 @@
+<?php 
+    $page = 'case-study/case-6';
+    $page_title = 'The Advantages of the Dokodemo-Kerja HR Application’s Screenshot Feature';
+
+    $meta_description = 'The Dokodemo-Kerja HR application is continually improving the quality of its overall functions and capabilities. The latest advantage is its incorporated monitoring abilities through a more accurate screenshot feature.';
+    $meta_keyword = 'dokodemo, dokodemo-kerja, dokodemo case, dokodemo story, accounting firm tool,  logique digital Indonesia, THR, employee monitoring, HR management, management tool, HR department';
+
+    $og_title = 'The Advantages of the Dokodemo-Kerja HR Application’s Screenshot Feature';
+    $og_image = 'https://dokodemo-kerja.com/img/og-img/case-study.jpg';
+    $og_desription = 'The Dokodemo-Kerja HR application is continually improving the quality of its overall functions and capabilities. The latest advantage is its incorporated monitoring abilities through a more accurate screenshot feature.';
+
+    $add_css = '<link rel="stylesheet" href="/css/style-case-study.css" />';
+
+    include($_SERVER['DOCUMENT_ROOT'].'/component/header.php');
+?>
+
+<div class="">
+    <section>
+        <div class="container">
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb breadcrumb--main">
+                    <li class="breadcrumb-item"><a href="/">Home</a></li>
+                    <li class="breadcrumb-item"><a href="/case-study/">Case Study</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">Case-6</li>
+                </ol>
+            </nav>
+        </div>
+    </section>
+
+    <section class="mb-5">
+        <div class="container">
+            <h1 class="text-center mb-5">Dokodemo-Kerja Case Study #6: <br>The Effective New Feature of Version 2.1.3, with its Ability to Accurately Detect Duplicate Screenshots</h1>
+            <div class="case-study__story">
+                <blockquote class="blockquote text-center mb-5">
+                    <p class="mb-0">“In order to use Dokodemo-kerja effectively, employees are required to use the application the way it was intended. When the user forgets to press the "Stop" button during a break, it can cause multiple screenshots to be taken sequentially, and thereby distort the total number of recorded working hours for that day. Therefore, we recommend that the administrator immediately contact the user so as to confirm if there are 3 successive identical screenshots taken by the system; some new features available on Dokodemo-kerja can make this confirmation process a lot easier.”</p>
+                    <!-- <div class="blockquote-footer"><cite title="Source Title">Operation Director</cite></div> -->
+                </blockquote>
+               
+                <div class="row">
+                    <div class="offset-md-1 col-md-10">
+                        <section class="text-justify">
+                            <div class="mb-4">
+                                <h2>
+                                    <b>
+                                        The Advantages of Dokodemo-Kerja's New Features
+                                    </b>
+                                </h2>
+                                <p>
+                                    For companies that are in charge of many employees, of course, it will be quite difficult to do this type of management (dynamically monitor employee screenshots). Therefore, we have developed a new feature that can automatically recognize and notify the user whenever the system detects the same 3 screenshots.
+                                </p>
+                            </div>
+                            <div class="mb-4">
+                                <picture>
+                                    <source media="(min-width: 1024px)" srcset="/img/case-study/6/image-1.jpg">
+                                    <source media="(max-width: 640px)" srcset="/img/case-study/6/image-1.jpg">
+                                    <img style="border-radius: 5px; box-shadow:1px 3px 3px gray;" width="100%" height="auto" src="/img/case-study/6/image-1.jpg" alt="banner">
+                                </picture> 
+                                <p class="mt-3">
+                                    When there are three or more rows of the same image, the image will then be marked in red and a notification will be sent to the administrator.
+                                </p>
+                            </div>
+                            
+                            <div class="mb-4">
+                                <picture>
+                                    <source media="(min-width: 1024px)" srcset="/img/case-study/6/image-2.jpg">
+                                    <source media="(max-width: 640px)" srcset="/img/case-study/6/image-2.jpg">
+                                    <img style="border-radius: 5px; box-shadow:1px 3px 3px gray;" width="100%" height="auto" src="/img/case-study/6/image-1.jpg" alt="banner">
+                                </picture> 
+                                <p class="mt-3">
+                                    After getting a confirmation from the administrator, and it further becomes clear that the employee in question was engaged in other types of work (eg. Being on the phone with a client), regardless of the screen remaining stagnant with Dokodemo-Kerja still on, then you don't need to take any action. However, if you do forget to press the "Stop" button then you will need to edit your working hours within the system in order to restore your proper working hours.
+                                </p>
+                                <p>
+                                    This is a new small feature that was added, and is specifically useful in aiding administrators when it comes to monitoring employee work hours.
+                                </p>
+                                <p>
+                                    If you haven't used it by now, we'd really appreciate it if you could check it out using the demo version. Do not hesitate to contact us.
+                                </p>
+                            </div>
+                            <div class="mb-4">
+                                <h2>
+                                    <b>                                        
+                                        Regularly Improving Dokodemo-Kerja's Functionality
+                                    </b>
+                                </h2>
+                                <p>
+                                    We regularly update so as to improve the functionality of Dokodemo-Kerja, starting from the screenshot, leave request & late management features, among many others. We hope that with our continuous developments, Dokodemo-Kerja can optimally meet and exceed  the basic needs of both the company and employee alike, making HRIS solutions a more than viable option for everyone.
+                                </p>
+                            </div>
+                            <hr />
+                            <div class="mb-4 text-center">
+                                <p>
+                                    Interested in knowing Dokodemo-Kerja's other features?
+                                </p>                               
+                                <a href="https://dokodemo-kerja.com/ind/fitur">                            
+                                    <button type="button" class="btn btn-sm btn-primary text-center">                                    
+                                       See More Dokodemo-Kerja Features here !
+                                    </button>
+                                </a>
+                            </div>
+                        </section>
+                    </div>
+                </div>                    
+            </div>
+        </div>
+    </section>
+</div>
+
+<?php include($_SERVER['DOCUMENT_ROOT'].'/ind/case-study/other-case.php'); ?>
+<?php include($_SERVER['DOCUMENT_ROOT'].'/ind/component/demo-banner.php'); ?>
+<?php include($_SERVER['DOCUMENT_ROOT'].'/ind/component/footer.php'); ?>
+<?php include($_SERVER['DOCUMENT_ROOT'].'/component/footer-end.php') ?>
